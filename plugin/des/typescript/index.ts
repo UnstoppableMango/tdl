1e216d4bf1f0c9ea4669686bf6d237ccb0ad7462
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-04-24 21:59:25 -0500

    Style

--- plugin/des/typescript/index.ts
+++ plugin/des/typescript/index.ts
@@ -1,12 +1,12 @@
 import * as http from 'node:http';
 import { connectNodeAdapter } from "@connectrpc/connect-node";
-import {ConnectRouter, HandlerContext} from "@connectrpc/connect";
-import {Greeter, HelloReply, HelloRequest} from "@unmango/tdl-es";
+import { ConnectRouter, HandlerContext } from "@connectrpc/connect";
+import { Greeter, HelloReply, HelloRequest } from "@unmango/tdl-es";
 
 const routes = (router: ConnectRouter) => {
 	router.service(Greeter, {
 		async sayHello(req: HelloRequest, context: HandlerContext) {
-			return new HelloReply({ });
+			return new HelloReply({});
 		},
 	});
 }
