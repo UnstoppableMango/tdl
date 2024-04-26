29c3578cc8811f325db621396cf1b8bd6fa9440f
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-04-26 16:47:55 -0500

    Bun stuff!

--- plugin/des/typescript/index.ts
+++ plugin/des/typescript/index.ts
@@ -1,16 +1,1 @@
-import * as http from 'node:http';
-import { connectNodeAdapter } from "@connectrpc/connect-node";
-import { ConnectRouter, HandlerContext } from "@connectrpc/connect";
-import { Greeter, HelloReply, HelloRequest } from "@unmango/tdl-es";
-
-const routes = (router: ConnectRouter) => {
-	router.service(Greeter, {
-		async sayHello(req: HelloRequest, context: HandlerContext) {
-			return new HelloReply({});
-		},
-	});
-}
-
-http.createServer(
-	connectNodeAdapter({ routes }),
-).listen(8080);
+console.log("Hello via Bun!");
