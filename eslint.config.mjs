4c5bdda03ae7cac03a08b79cdc4374fb10f0494a
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-04-27 14:08:35 -0500

    Semi-working config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,27 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tslint from "typescript-eslint";
 
 export default tslint.config(
 	eslint.configs.recommended,
-	...tslint.configs.recommended,
+	...tslint.configs.recommendedTypeChecked,
+	{
+		ignores: [
+			".config/",
+			".idea/",
+			".make",
+			".vscode/",
+			"bin/",
+			"gen/",
+			"obj/",
+			"proto/",
+		],
+		languageOptions: {
+			parserOptions: {
+				project: ["tsconfig.eslint.json", "plugin/gen/*/tsconfig.json"],
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
 );
