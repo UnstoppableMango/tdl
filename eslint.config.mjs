cfcb132b700975d7dad2bf48d69ba6bb68e21cef
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-04-27 13:32:35 -0500

    Configuring eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tslint from 'typescript-eslint';
+
+export default tslint.config(
+	eslint.configs.recommended,
+	...tslint.configs.recommended,
+);
