684d493bd4dbdbf3653465b7ca4de6872ee61ede
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-04-29 23:41:47 -0500

    Little tidbits in the TS

--- packages/ts/gen.ts
+++
@@ -1,7 +0,0 @@
-import type { Spec } from '@unmango/tdl-ts';
-import ts from 'typescript';
-
-export function gen(spec: Spec): ts.NodeArray<ts.Node> {
-	const node = ts.factory.createInterfaceDeclaration([], 'Test', undefined, undefined, []);
-	return ts.factory.createNodeArray([node]);
-}
--- packages/ts/generator.ts
+++ packages/ts/generator.ts
@@ -2,15 +2,12 @@ import * as tdl from '@unmango/tdl-ts';
 import * as uml from '@unmango/uml';
 import { Writable } from 'node:stream';
 import ts from 'typescript';
-import { gen } from './gen';
 
 export class Generator implements uml.Generator {
 	gen(spec: tdl.Spec, writer: Writable): Promise<void> {
-		const nodes = gen(spec);
-
 		const source = ts.createSourceFile('types.d.ts', '', ts.ScriptTarget.ES2019, undefined, ts.ScriptKind.TS);
 		const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
-		const result = printer.printList(ts.ListFormat.MultiLine, nodes, source);
+		const result = printer.printList(ts.ListFormat.MultiLine, gen(spec), source);
 
 		const encoding: BufferEncoding = 'utf-8';
 		const bytes = Buffer.from(result, encoding);
@@ -19,4 +16,13 @@ export class Generator implements uml.Generator {
 
 		return Promise.resolve();
 	}
+}
+
+function gen(spec: tdl.Spec): ts.NodeArray<ts.Node> {
+	const types = Object.entries(spec.types).map(x => genType(...x));
+	return ts.factory.createNodeArray(types);
+}
+
+function genType(name: string, type: tdl.Type): ts.Node {
+	return ts.factory.createInterfaceDeclaration([], name, undefined, undefined, []);
 }
