5404d3c919e3d417b8860baad1b51f0c9a49d2b0
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-01 01:16:28 -0500

    Refactor generator test for simplicity
    The generator test has been simplified by removing the use of a Writable stream. The 'gen' function now directly returns the result, which is then tested for null and emptiness. This change makes the test more straightforward and easier to understand.

--- packages/ts/generator.spec.ts
+++ packages/ts/generator.spec.ts
@@ -1,6 +1,5 @@
 import * as tdl from '@unmango/tdl-es';
 import { describe, expect, it } from 'bun:test';
-import { Writable } from 'node:stream';
 import { generator } from '.';
 
 describe('Generator', () => {
@@ -21,16 +20,9 @@ describe('Generator', () => {
 			version: '0.1.0',
 		});
 
-		await generator.gen(
-			spec,
-			new Writable({
-				write: (chunk) => sink.write(chunk),
-			}),
-		);
+		const actual = await generator.gen(spec);
 
-		const decoder = new TextDecoder();
-		const actual = decoder.decode(sink.end());
 		expect(actual).not.toBeNull();
 		expect(actual).toBeEmpty();
 	});
-})
+});
