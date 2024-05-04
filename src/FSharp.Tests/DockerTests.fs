e688562784910a9801d05c56e8992e1decf2f9de
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-04 14:58:19 -0500

    Added FSharp test project and Docker tests
    A new FSharp test project has been added to the solution. This includes a new DockerTests.fs file, which contains tests for performing IO with a container. A Program.fs file was also created as an entry point for the test project. The solution file has been updated to include this new project and its configurations.

---
+++ src/FSharp.Tests/DockerTests.fs
@@ -0,0 +1,37 @@
+﻿module DockerTests
+
+open System
+open System.IO
+open System.Text
+open Docker.DotNet
+open FsCheck.FSharp
+open FsCheck.Xunit
+open UnMango.Tdl
+
+type Message =
+  static member Double() =
+    ArbMap.defaults
+    |> ArbMap.arbitrary<string>
+    |> Arb.filter (fun s -> s.Length > 0 && not <| s.Contains('\\'))
+
+let rand = Random()
+
+let testWorkload = Docker.Workload.create $"tdl-test-{rand.Next()}"
+
+[<Property(Arbitrary = [| typeof<Message> |])>]
+let ``Can perform IO with a container`` (message: string) =
+  async {
+    use input = new MemoryStream(Encoding.UTF8.GetBytes(message), false)
+    use output = new MemoryStream()
+    use config = new DockerClientConfiguration()
+    use client = config.CreateClient()
+
+    let workload =
+      { testWorkload "ubuntu" "latest" with
+          Entrypoint = [ "cat" ] }
+
+    do! client |> Docker.exec workload input output
+    let actual = Encoding.UTF8.GetString(output.ToArray())
+
+    return actual = message
+  }
