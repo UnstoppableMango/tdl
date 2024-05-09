88e1945a4282bfab4fc88092c8987d5320995a4e
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-09 01:08:59 -0500

    Updated import and interface definitions
    The import statement in the converter.ts file has been updated to use '@unmango/tdl-es' instead of '@unmango/tdl-ts'. The 'to' method and related interfaces have been removed from both files. The 'gen' method in the Generator interface now returns a Promise<void> instead of Promise<string>, and takes an additional 'writer' parameter.

--- packages/uml/index.ts
+++ packages/uml/index.ts
@@ -1,18 +1,12 @@
 import * as tdl from '@unmango/tdl-es';
 import { Readable, Writable } from 'node:stream';
 
-export interface ConverterFrom {
+export interface Converter {
 	from(reader: Readable): Promise<tdl.Spec>;
 }
 
-export interface ConverterTo {
-	to(spec: tdl.Spec, writer: Writable): Promise<void>;
-}
-
-export interface Converter extends ConverterFrom, ConverterTo {}
-
 export interface Generator {
-	gen(spec: tdl.Spec): Promise<string>;
+	gen(spec: tdl.Spec, writer: Writable): Promise<void>;
 }
 
 export const SUPPORTED_MIME_TYPES = [
