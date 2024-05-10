6715873e76a43f37cc60a4a7de992c4c073e8e5f
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-10 11:12:14 -0500

    Add a comment about the annoyance

--- src/Broker/Program.cs
+++ src/Broker/Program.cs
@@ -4,9 +4,10 @@ using UnMango.Tdl.Broker.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 var socket = builder.Environment.IsDevelopment()
-	? Path.Combine(Path.GetTempPath(), "tdl.sock")
+	? Path.Combine(Path.GetTempPath(), "broker.sock")
 	: "/var/run/tdl/broker.sock";
 
+// Would be nice to get rid of the "Overriding address(es) warning"
 builder.WebHost.ConfigureKestrel(kestrel => {
 	kestrel.ListenUnixSocket(socket, listen => {
 		listen.Protocols = HttpProtocols.Http2;
