da0ee24d13259fae9df454a08c5fe7d9d129183b
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-11 16:33:26 -0500

    Refined command descriptions in CLI
    The commit includes updates to the descriptions of various commands and arguments in the CLI. The changes primarily involve removing periods at the end of these descriptions, making them more concise and consistent. Both 'From' and 'Gen' commands have been updated accordingly.

--- src/Cli/Commands.cs
+++ src/Cli/Commands.cs
@@ -17,12 +17,12 @@ file class TokenBinder : BinderBase<CancellationToken>
 internal static class Commands
 {
 	public static Command From() {
-		var sourceArg = new Argument<string>("source", "The source format.");
-		var toOpt = new Option<string?>(["--to", "-t", "-2"], "The target format.");
-		var fileArg = new Argument<IEnumerable<FileInfo>>("file", "The file(s) to convert.") {
+		var sourceArg = new Argument<string>("source", "The source format");
+		var toOpt = new Option<string?>(["--to", "-t", "-2"], "The target format");
+		var fileArg = new Argument<IEnumerable<FileInfo>>("file", "The file(s) to convert") {
 			Arity = ArgumentArity.ZeroOrMore,
 		};
-		var command = new Command("from", "Convert the source format to uml.") {
+		var command = new Command("from", "Convert the source format to uml") {
 			sourceArg, toOpt, fileArg,
 		};
 		command.SetHandler(Handlers.From, sourceArg, toOpt, fileArg, TokenBinder.Value);
@@ -30,12 +30,12 @@ internal static class Commands
 	}
 
 	public static Command Gen() {
-		var targetArg = new Argument<string>("target", "The target output format.");
-		var sourceArg = new Argument<string>("source", "The source format.");
-		var fileArg = new Argument<IEnumerable<FileInfo>>("file", "The file(s) to convert.") {
+		var targetArg = new Argument<string>("target", "The target output format");
+		var sourceArg = new Argument<string>("source", "The source format");
+		var fileArg = new Argument<IEnumerable<FileInfo>>("file", "The file(s) to convert") {
 			Arity = ArgumentArity.ZeroOrMore,
 		};
-		var command = new Command("generate", "Generate types for the target format.") {
+		var command = new Command("generate", "Generate types for the target format") {
 			sourceArg, fileArg,
 		};
 		command.AddAlias("gen");
