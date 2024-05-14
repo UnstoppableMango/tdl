d05eaac186695fbcb6e2f5893f59322af0544a88
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-14 11:51:04 -0500

    Make it compile

--- src/Abstractions/Runner.cs
+++ src/Abstractions/Runner.cs
@@ -3,12 +3,12 @@ namespace UnMango.Tdl.Abstractions;
 [PublicAPI]
 public class Runner(IConverter converter, IGenerator generator) : IRunner
 {
-	public virtual Task<Spec> FromAsync(ISource source, CancellationToken cancellationToken = default) {
-		return converter.FromAsync(source, cancellationToken);
+	public virtual Task<Spec> FromAsync(Stream input, CancellationToken cancellationToken = default) {
+		return converter.FromAsync(input, cancellationToken);
 	}
 
-	public virtual Task GenerateAsync(Spec spec, ITarget target, CancellationToken cancellationToken = default) {
-		return generator.GenerateAsync(spec, target, cancellationToken);
+	public virtual Task GenerateAsync(Spec spec, Stream output, CancellationToken cancellationToken = default) {
+		return generator.GenerateAsync(spec, output, cancellationToken);
 	}
 
 	public Runner With(IConverter next) => new(next, generator);
