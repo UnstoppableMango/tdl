d6b5abd488942ca1f6e0c17e054898c30e86a5d7
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-05-16 16:11:08 -0500

    Accept the plugin as an argument

--- src/FSharp/CliRunner.fs
+++ src/FSharp/CliRunner.fs
@@ -7,27 +7,27 @@ open UnMango.Tdl.Abstractions
 open UnMango.Tdl.Tdl
 
 module CliRunner =
-  let from: From =
+  let from plugin : From =
     fun input -> async {
       let pluginDir = Config.Env |> Config.pluginDir
-      let plugin = Path.Combine(pluginDir, source.Plugin)
+      let plugin = Path.Combine(pluginDir, plugin)
       use stream = new MemoryStream()
 
       let! _ = command plugin {
         args [ "from" ]
-        stdin source.Input
+        stdin input
         stdout (PipeTo.stream stream)
         async
       }
 
       stream.Position <- 0
-      return stream |> Spec.Parser.ParseFrom |> Ok
+      return stream |> Spec.Parser.ParseFrom
     }
 
-  let gen: Gen =
-    fun spec target -> async {
+  let gen plugin : Gen =
+    fun spec output -> async {
       let pluginDir = Config.Env |> Config.pluginDir
-      let plugin = Path.Combine(pluginDir, target.Plugin)
+      let plugin = Path.Combine(pluginDir, plugin)
       use stream = new MemoryStream()
       spec.WriteTo(stream)
       stream.Position <- 0
@@ -35,17 +35,17 @@ module CliRunner =
       let! _ = command plugin {
         args [ "gen" ]
         stdin stream
-        stdout (PipeTo.stream target.Output)
+        stdout (PipeTo.stream output)
         async
       }
 
-      return Ok()
+      return ()
     }
 
-type CliRunner() =
+type CliRunner(plugin) =
   interface IRunner with
     member this.FromAsync(input, cancellationToken) =
-      Async.StartAsTask(CliRunner.from input, cancellationToken = cancellationToken)
+      Async.StartAsTask(CliRunner.from plugin input, cancellationToken = cancellationToken)
 
     member this.GenerateAsync(input, output, cancellationToken) =
-      Async.StartAsTask(CliRunner.gen input output, cancellationToken = cancellationToken)
+      Async.StartAsTask(CliRunner.gen plugin input output, cancellationToken = cancellationToken)
