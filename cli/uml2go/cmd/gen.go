6ddc41906fd997c45f3b0d9fd35f48ae152c7608
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-04 16:53:33 -0500

    Implement the CLI in go
    Not sure if I'm going to pivot yet, but this could be an improvement over the .NET CLI

--- cli/uml2go/cmd/gen.go
+++ cli/uml2go/cmd/gen.go
@@ -3,10 +3,13 @@ package cmd
 import (
 	cli "github.com/unstoppablemango/tdl/cli/internal"
 	gen "github.com/unstoppablemango/tdl/pkg/go"
+	"github.com/unstoppablemango/tdl/pkg/uml"
 )
 
 func init() {
 	rootCmd.AddCommand(genCmd)
 }
 
-var genCmd = cli.NewGenCmd(gen.Go)
+var genCmd = cli.NewGenCmd(func(_ uml.GeneratorOptions) uml.Generator {
+	return gen.Go
+})
