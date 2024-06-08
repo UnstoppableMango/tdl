c7fdc531e77f97e4b0258d8ce754fb5585ad4f44
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-08 11:23:48 -0500

    It... works???

--- cli/internal/cmd.go
+++ cli/internal/cmd.go
@@ -1,37 +1,54 @@
 package cli
 
 import (
-	"fmt"
+	"io"
 	"os"
 
 	"github.com/spf13/cobra"
 	"github.com/unstoppablemango/tdl/pkg/uml"
+	"google.golang.org/protobuf/proto"
 )
 
 func NewFromCmd(create uml.NewConverter) *cobra.Command {
 	return &cobra.Command{
-		Use:  "from",
-		Args: cobra.ExactArgs(1),
+		Use: "from",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			opts := uml.ConverterOptions{}
-			uml.Apply(&opts, uml.WithMimeType(args[0]))
-			conv := create(opts)
-			spec, err := conv.From(cmd.Context(), os.Stdin)
-			fmt.Printf("output:\n%s\n", spec)
-			return err
+			spec, err := create(opts).From(cmd.Context(), os.Stdin)
+			if err != nil {
+				return err
+			}
+
+			data, err := proto.Marshal(spec)
+			if err != nil {
+				return err
+			}
+
+			if _, err = os.Stdout.Write(data); err != nil {
+				return err
+			}
+
+			return nil
 		},
 	}
 }
 
 func NewGenCmd(create uml.NewGenerator) *cobra.Command {
 	return &cobra.Command{
-		Use:  "gen",
-		Args: cobra.ExactArgs(1),
+		Use: "gen",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			opts := uml.GeneratorOptions{}
-			uml.Apply(&opts, uml.WithTarget(args[0]))
-			gen := create(opts)
-			return gen.Gen(cmd.Context(), &uml.Spec{}, os.Stdout)
+			data, err := io.ReadAll(os.Stdin)
+			if err != nil {
+				return err
+			}
+
+			spec := uml.Spec{}
+			if err = proto.Unmarshal(data, &spec); err != nil {
+				return err
+			}
+
+			return create(opts).Gen(cmd.Context(), &spec, os.Stdout)
 		},
 	}
 }
