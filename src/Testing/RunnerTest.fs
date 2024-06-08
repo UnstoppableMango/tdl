7c54ef59c2722f4f046f4cf89df385a3a4f8db43
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-07 23:26:07 -0500

    Updated test functions and added new ones
    Enhanced the 'roundTrip' function in the testing module to use 'Equals' method for comparison instead of equality operator. Added two new async functions, 'generateData' and 'consumeData', to respectively generate data from a given spec into a stream and consume data from a stream into a spec, checking if the consumed spec equals the original one.

--- src/Testing/RunnerTest.fs
+++ src/Testing/RunnerTest.fs
@@ -1,6 +1,7 @@
 module UnMango.Tdl.Testing.RunnerTest
 
 open System.IO
+open Google.Protobuf
 open UnMango.Tdl
 
 let roundTrip (gen: Tdl.Gen, from: Tdl.From) spec = async {
@@ -8,5 +9,16 @@ let roundTrip (gen: Tdl.Gen, from: Tdl.From) spec = async {
   do! gen spec stream
   stream.Position <- 0
   let! result = from stream
-  return result = spec
+  return result.Equals(spec)
+}
+
+let generateData (gen: Tdl.Gen, _: Tdl.From) spec = async {
+  use stream = new MemoryStream()
+  do! gen spec stream
+  return stream.Length > 0
+}
+
+let consumeData (_: Tdl.Gen, from: Tdl.From) (spec: Spec) (stream: MemoryStream) = async {
+  let! result = from stream
+  return result.Equals(spec)
 }
