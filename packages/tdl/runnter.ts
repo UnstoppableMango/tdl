5dd4455ec782b94e268941bc8e836a9ec8669a79
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-09 03:13:39 -0500

    WIP the TS cli runner

--- packages/tdl/runnter.ts
+++ packages/tdl/runnter.ts
@@ -1,14 +1,18 @@
 import { Spec } from '@unmango/tdl-es';
+import type { BunFile } from 'bun';
 import type { Runner } from '.';
 
-export const cliRunner: Runner = {
-	async from(reader): Promise<Spec> {
+export class CliRunner implements Runner<Spec> {
+	constructor(private path: string) {}
+	async from(reader: BunFile): Promise<Spec> {
 		const input = await reader.arrayBuffer();
 		const data = new Uint8Array(input);
+		const proc = Bun.spawn([this.path, 'from']);
+		await Bun.write(proc.stdin, data);
 		return Spec.fromBinary(data);
-	},
-	async gen(spec, writer): Promise<void> {
+	}
+	async gen(spec: Spec, writer: BunFile): Promise<void> {
 		const data = spec.toBinary();
 		await Bun.write(writer, data);
 	}
-};
+}
