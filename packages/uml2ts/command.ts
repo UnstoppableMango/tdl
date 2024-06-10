6bcf3168f5b475cf27fb1436a2e8450bab2a0b41
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-09 22:36:53 -0500

    Fix lint

--- packages/uml2ts/command.ts
+++ packages/uml2ts/command.ts
@@ -1,9 +1,8 @@
-import { generator } from '@unmango/2ts';
+import { gen as generate } from '@unmango/2ts';
 import * as uml from '@unmango/uml';
 
 export async function gen(type?: uml.SupportedMimeType): Promise<void> {
 	const buffer = await Bun.stdin.arrayBuffer();
 	const spec = uml.read(new Uint8Array(buffer), type);
-	await generator.gen(spec, Bun.stdout);
-	await Bun.write(Bun.stdout, ts);
+	await generate(spec, Bun.stdout);
 }
