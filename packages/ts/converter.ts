180cb267a8fb2684afa45c4342ede24618a37736
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-11 21:58:14 -0500

    Working on the converter for some reason

--- packages/ts/converter.ts
+++ packages/ts/converter.ts
@@ -1,7 +1,16 @@
 import * as tdl from '@unmango/tdl';
 import type { Spec } from '@unmango/tdl-es';
 import type { BunFile } from 'bun';
+import ts from 'typescript';
 
 export const from: tdl.From<Spec> = async (reader: BunFile): Promise<Spec> => {
+	const data = await reader.arrayBuffer();
+	const content = new TextDecoder().decode(data);
+	const source = ts.createSourceFile('stdin', content, ts.ScriptTarget.ES2019, undefined, ts.ScriptKind.TS);
+	source.forEachChild(parseNode);
 	return await Promise.reject('not implemented');
 };
+
+function parseNode(node: ts.Node): void {
+	throw new Error('not implemented');
+}
