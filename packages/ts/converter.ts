c465e1d7167b972e052783bc000e0f5c6fe7d24f
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-11 22:53:56 -0500

    WIP on converter tests

--- packages/ts/converter.ts
+++ packages/ts/converter.ts
@@ -1,5 +1,5 @@
 import * as tdl from '@unmango/tdl';
-import type { Spec } from '@unmango/tdl-es';
+import { Spec, Type } from '@unmango/tdl-es';
 import type { BunFile } from 'bun';
 import ts from 'typescript';
 
@@ -7,10 +7,26 @@ export const from: tdl.From<Spec> = async (reader: BunFile): Promise<Spec> => {
 	const data = await reader.arrayBuffer();
 	const content = new TextDecoder().decode(data);
 	const source = ts.createSourceFile('stdin', content, ts.ScriptTarget.ES2019, undefined, ts.ScriptKind.TS);
-	source.forEachChild(parseNode);
+	const parts = source.forEachChild(parseNode);
 	return await Promise.reject('not implemented');
 };
 
-function parseNode(node: ts.Node): void {
-	throw new Error('not implemented');
+function parseNode(node: ts.Node): Spec {
+	switch (node.kind) {
+		case ts.SyntaxKind.InterfaceDeclaration:
+			return parseInterface(node as ts.InterfaceDeclaration);
+	}
+
+	return new Spec();
+}
+
+function parseInterface(node: ts.InterfaceDeclaration): Spec {
+	const type = new Type();
+	const name = node.name.text;
+
+	return new Spec({
+		types: {
+			[name]: type,
+		},
+	});
 }
