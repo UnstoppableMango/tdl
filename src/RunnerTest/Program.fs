fd60b794ef1f6be40edc3916c523831ddae3e615
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-12 20:16:33 -0500

    Set a higher max test in CI (#160)
    * Add max test config
    
    * Updated test configuration based on environment
    
    The number of maximum tests to run is now determined by the presence of a "CI" environment variable. If the variable is not set or empty, 100 tests will be run; otherwise, it will run 500 tests. This change allows for more extensive testing in CI environments while keeping local testing relatively quick.

--- src/RunnerTest/Program.fs
+++ src/RunnerTest/Program.fs
@@ -8,6 +8,11 @@ open FsCheck.FSharp
 open UnMango.Tdl
 open UnMango.Tdl.Testing
 
+let maxTest =
+  match Environment.GetEnvironmentVariable("CI") with
+  | ci when String.IsNullOrWhiteSpace(ci) -> 100
+  | _ -> 500
+
 let validatePath (arg: Argument<FileInfo>) (result: SymbolResult) : unit =
   match result.GetValueForArgument(arg) with
   | null -> result.ErrorMessage <- "<path> is required"
@@ -18,6 +23,7 @@ let run (bin: FileInfo) =
   let from = CliRunner.from bin.FullName
   let gen = CliRunner.gen bin.FullName
   let spec = ArbMap.defaults |> TdlArbs.merge |> ArbMap.arbitrary<Spec>
+  let config = Config.Quick.WithMaxTest(maxTest)
 
   Console.WriteLine("Executing runner test suite")
 
@@ -25,7 +31,7 @@ let run (bin: FileInfo) =
     "Should generate data", RunnerTest.generateData ]
   |> List.map (fun (n, t) -> n, t (gen, from))
   |> List.map (fun (n, t) -> n, Prop.forAll spec t)
-  |> List.map Check.Quick
+  |> List.map (fun (n, t) -> Check.One(n, config, t))
   |> ignore
 
 [<EntryPoint>]
