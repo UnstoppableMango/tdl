0f3b6d1bdf0c9413ae9a48fc026da2a76f3bc154
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-15 22:59:43 -0500

    Enhanced testing for CLI runner
    Expanded the test coverage for the CLI runner in the 'runner' package. Added new tests to validate path usage, marshalling and unmarshalling of specs, and execution of runners. Also updated some existing tests to improve error handling.

--- pkg/runner/cli_test.go
+++ pkg/runner/cli_test.go
@@ -1,20 +1,24 @@
 package runner
 
 import (
+	"bytes"
+	"context"
+	"io"
 	"os"
 	"path"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/unstoppablemango/tdl/pkg/uml"
+	"google.golang.org/protobuf/proto"
 )
 
 var (
 	binDir = os.Getenv("BIN_DIR")
-	bin    = path.Join(binDir, "um")
+	bin    = path.Join(binDir, "go_echo")
 )
 
-var _ = Describe("NewCli", func() {
+var _ = Describe("runner/cli", func() {
 	var runner uml.Runner
 	var cliRunner *cli
 	var err error
@@ -24,13 +28,59 @@ var _ = Describe("NewCli", func() {
 		cliRunner = runner.(*cli)
 	})
 
-	It("should create a CLI runner", func() {
-		Expect(err).To(BeNil())
-		Expect(runner).NotTo(BeNil())
+	Describe("NewCli", func() {
+		It("should create a CLI runner", func() {
+			Expect(err).NotTo(HaveOccurred())
+			Expect(runner).NotTo(BeNil())
+		})
+
+		It("should use the provided path", func() {
+			Expect(err).NotTo(HaveOccurred())
+			Expect(cliRunner.Path).To(Equal(bin))
+		})
 	})
 
-	It("should use the provided path", func() {
-		Expect(err).To(BeNil())
-		Expect(cliRunner.Path).To(Equal(bin))
+	Describe("From", func() {
+		It("should work", func() {
+			Expect(err).NotTo(HaveOccurred())
+
+			By("marshalling an arbitrary spec")
+			input := uml.Spec{Name: "testing"}
+			data, err := proto.Marshal(&input)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("creating a byte reader")
+			reader := bytes.NewReader(data)
+
+			By("executing the runner")
+			spec, err := cliRunner.From(context.Background(), reader)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(spec).To(Equal(&input))
+		})
+	})
+
+	Describe("Gen", func() {
+		It("should work", func() {
+			Expect(err).NotTo(HaveOccurred())
+
+			input := uml.Spec{Name: "testing"}
+			buf := bytes.Buffer{}
+
+			By("executing the runner")
+			err = cliRunner.Gen(context.Background(), &input, &buf)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("reading from the buffer")
+			data, err := io.ReadAll(&buf)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("unmarshalling the result")
+			spec := uml.Spec{}
+			err = proto.Unmarshal(data, &spec)
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(&spec).To(Equal(&input))
+		})
 	})
 })
