536a83febf33395ee1720767e7d4b2e07067d1a8
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-18 09:41:56 -0500

    Refactor the wrappers

--- src/FSharp/Tdl.fs
+++ src/FSharp/Tdl.fs
@@ -3,8 +3,7 @@ module UnMango.Tdl.Tdl
 open System.IO
 open UnMango.Tdl.Abstractions
 
-type TdlError =
-  | Message of string
+type TdlError = Message of string
 
 type FromResult = Result<Spec, TdlError>
 type GenResult = TdlError option
@@ -23,14 +22,26 @@ module From =
   let wrap (c: IConverter) : From =
     fun o -> async {
       let! ct = Async.CancellationToken
-      return! c.FromAsync(o, ct) |> Async.AwaitTask
+
+      try
+        let! res = c.FromAsync(o, ct) |> Async.AwaitTask
+        return Ok res
+      with ex ->
+        return (Message ex.Message |> Error)
     }
 
 module Gen =
+  open System
+
   let wrap (g: IGenerator) : Gen =
     fun i o -> async {
       let! ct = Async.CancellationToken
-      return! g.GenerateAsync(i, o, ct) |> Async.AwaitTask
+
+      try
+        do! g.GenerateAsync(i, o, ct) |> Async.AwaitTask
+        return None
+      with ex ->
+        return (Message ex.Message |> Some)
     }
 
 module Runner =
