5c9c13c681b67a5f4bc3770f040a9652c48e32f0
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-18 23:02:08 -0500

    Refactor error handling in Apply function
    The Apply function has been refactored to improve error handling. Instead of panicking when an error occurs, it now returns the error. The function signature has also been updated to return a pointer and an error.

--- pkg/uml/options.go
+++ pkg/uml/options.go
@@ -4,14 +4,14 @@ type Opt[T any] interface {
 	~func(*T) error
 }
 
-func Apply[T Opt[V], V any](options *V, opts ...T) *V {
+func Apply[T Opt[V], V any](options V, opts ...T) (*V, error) {
 	for _, opt := range opts {
-		if err := opt(options); err != nil {
-			panic(err)
+		if err := opt(&options); err != nil {
+			return nil, err
 		}
 	}
 
-	return options
+	return &options, nil
 }
 
 func Flat[T Opt[V], V any](opts ...T) T {
