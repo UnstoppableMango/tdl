28c1221beed0ca4664c725bb595234d8e64861d4
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-19 16:23:38 +0000

    Add logging to the go CLI runner (#186)
    * Refactor Apply function to return options
    
    The Apply function in the UML package has been refactored. It now returns the modified options instead of void. This change allows for more flexible usage and chaining of operations.
    
    * Refactor error handling in Apply function
    
    The Apply function has been refactored to improve error handling. Instead of panicking when an error occurs, it now returns the error. The function signature has also been updated to return a pointer and an error.
    
    * Refactor CLI constructor with options pattern
    
    The CLI constructor has been refactored to use the options pattern. This allows for more flexible configuration of the CLI instance. A new function `WithArgs` has been added to append arguments to a CLI instance. The changes improve code readability and maintainability by making it easier to add new configuration options in the future.
    
    * Refactor CLI options handling
    
    The code changes refactor the way CLI options are handled. The type definition for CliOpt has been updated to a function that returns an error. The NewCli and WithArgs functions have also been adjusted accordingly to match this new type definition. This change simplifies the usage of these functions and improves error handling.
    
    * Add logging to the CLI runner

--- pkg/runner/cli.go
+++ pkg/runner/cli.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"io"
+	"log/slog"
 	"os"
 	"os/exec"
 
@@ -14,32 +15,53 @@ import (
 type cli struct {
 	Path string
 	Args []string
+	log  *slog.Logger
 }
 
-func NewCli(path string, args ...string) (uml.Runner, error) {
+type CliOpt func(*cli) error
+
+func NewCli(path string, opts ...CliOpt) (uml.Runner, error) {
 	if _, err := os.Stat(path); err != nil {
 		return nil, err
 	}
 
-	return &cli{
+	return uml.Apply(cli{
 		Path: path,
-		Args: args,
-	}, nil
+		Args: []string{},
+		log:  slog.Default(),
+	}, opts...)
+}
+
+func WithArgs(arg ...string) CliOpt {
+	return func(c *cli) error {
+		c.Args = append(c.Args, arg...)
+		return nil
+	}
+}
+
+func WithLogger(log *slog.Logger) CliOpt {
+	return func(c *cli) error {
+		c.log = log
+		return nil
+	}
 }
 
 // From implements uml.Runner.
 func (c *cli) From(ctx context.Context, reader io.Reader) (*uml.Spec, error) {
 	args := append([]string{"from"}, c.Args...)
 	cmd := exec.Command(c.Path, args...)
+	c.log.Debug("built command", "path", c.Path, "args", args)
 
-	cmd.Stdin = reader
 	buf := &bytes.Buffer{}
+	cmd.Stdin = reader
 	cmd.Stdout = buf
 
+	c.log.Info("executing command")
 	if err := cmd.Run(); err != nil {
 		return nil, err
 	}
 
+	c.log.Debug("unmarshalling proto response")
 	spec := &uml.Spec{}
 	err := proto.Unmarshal(buf.Bytes(), spec)
 
@@ -48,6 +70,7 @@ func (c *cli) From(ctx context.Context, reader io.Reader) (*uml.Spec, error) {
 
 // Gen implements uml.Runner.
 func (c *cli) Gen(ctx context.Context, spec *uml.Spec, writer io.Writer) error {
+	c.log.Debug("marshalling input proto")
 	inData, err := proto.Marshal(spec)
 	if err != nil {
 		return err
@@ -55,10 +78,12 @@ func (c *cli) Gen(ctx context.Context, spec *uml.Spec, writer io.Writer) error {
 
 	args := append([]string{"gen"}, c.Args...)
 	cmd := exec.Command(c.Path, args...)
+	c.log.Debug("built command", "path", c.Path, "args", args)
 
 	cmd.Stdin = bytes.NewReader(inData)
 	cmd.Stdout = writer
 
+	c.log.Info("executing command")
 	return cmd.Run()
 }
 
--- pkg/uml/options.go
+++ pkg/uml/options.go
@@ -4,12 +4,14 @@ type Opt[T any] interface {
 	~func(*T) error
 }
 
-func Apply[T Opt[V], V any](options *V, opts ...T) {
+func Apply[T Opt[V], V any](options V, opts ...T) (*V, error) {
 	for _, opt := range opts {
-		if err := opt(options); err != nil {
-			panic(err)
+		if err := opt(&options); err != nil {
+			return nil, err
 		}
 	}
+
+	return &options, nil
 }
 
 func Flat[T Opt[V], V any](opts ...T) T {
