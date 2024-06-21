904960ebb4317e4e1f0b5ba5b9a01aa788c124d5
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-20 22:36:15 -0500

    Added plugin support to CLI commands
    The 'from' and 'gen' CLI commands have been updated to include plugin support. The changes involve getting the plugin name, looking up the plugin, and executing it with a runner. Error handling has also been added for each step of this process.

--- cli/um/cmd/from.go
+++ cli/um/cmd/from.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/spf13/cobra"
 	cli "github.com/unstoppablemango/tdl/cli/internal"
+	"github.com/unstoppablemango/tdl/pkg/plugin"
 	"github.com/unstoppablemango/tdl/pkg/runner"
 	"github.com/unstoppablemango/tdl/pkg/uml"
 )
@@ -16,7 +17,21 @@ func init() {
 
 var fromCmd = cli.NewFromCmd(
 	func(ctx context.Context, opts cli.FromCmdOptions, args []string) (uml.Converter, error) {
+		opts.Log.Debug("getting plugin name")
+		source, err := plugin.ForTarget(args[0])
+		if err != nil {
+			return nil, err
+		}
+
+		opts.Log.Debug("looking up plugin")
+		bin, err := plugin.LookupPath(source)
+		if err != nil {
+			return nil, err
+		}
+
 		opts.Log.Debug("executing cli runner")
-		return runner.NewCli("")
+		return runner.NewCli(bin,
+			runner.WithLogger(opts.Log),
+		)
 	},
 )
