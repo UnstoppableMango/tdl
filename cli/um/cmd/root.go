d2c2045049291af2adf830379fbc9ade62319d87
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-21 23:08:27 -0500

    Removed error logging in root command execution
    The error logging that was previously present in the root command execution has been removed. This change simplifies the code and relies on the system's default error handling instead of explicitly printing to stderr.

--- cli/um/cmd/root.go
+++ cli/um/cmd/root.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -18,7 +17,6 @@ var rootCmd = &cobra.Command{
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
