c60668e67b034ecba1f910786842df1887c800f4
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-21 22:32:22 -0500

    Revert "Accept an input file flag"
    This reverts commit 5e2b6860f5b8bcfce57b6aceead514a83af6ec8a.

--- cli/internal/options.go
+++
@@ -1,7 +0,0 @@
-package cli
-
-import "github.com/spf13/cobra"
-
-func InputFile(cmd *cobra.Command, file *string, usage string) {
-	cmd.PersistentFlags().StringVarP(file, "input", "i", "", usage)
-}
