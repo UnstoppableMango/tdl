ae9f5dd59fd1641cb97b87738e05fbea6d148eea
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-24 02:00:42 -0500

    WIP adding media type functions to the converter

--- pkg/uml/converter.go
+++ pkg/uml/converter.go
@@ -1,9 +1,12 @@
 package uml
 
 import (
+	"bytes"
 	"context"
 	"io"
 	"log/slog"
+
+	"github.com/unstoppablemango/tdl/pkg/result"
 )
 
 type ConverterOptions struct {
@@ -15,7 +18,7 @@ type ConverterOptions struct {
 type ConverterOption func(*ConverterOptions) error
 
 type From[T, V any] interface {
-	func(context.Context, T) (*V, error)
+	func(context.Context, T) result.R[V]
 }
 
 type Converter interface {
@@ -30,5 +33,38 @@ func WithMediaType(t string) ConverterOption {
 	return func(opts *ConverterOptions) error {
 		opts.MediaType = &t
 		return nil
+	}
+}
+
+func MapFromInput[A, B, C any, F From[A, C], R From[B, C]](f func(B) A, from F) R {
+	return func(ctx context.Context, input B) (C, error) {
+		return from(ctx, f(input))
+	}
+}
+
+func MapFromOutput[A, B, C any, F From[A, B], R From[A, C]](f func(B) C, from F) R {
+	return func(ctx context.Context, input A) (C, error) {
+		result, err := from(ctx, input)
+		if err != nil {
+			return nil, err
+		}
+
+		return f(result), nil
+	}
+}
+
+func FromMediaType[F From[io.Reader, *Spec], R From[io.Reader, io.Reader]](mediaType string, from F) R {
+	return func(ctx context.Context, reader io.Reader) (io.Reader, error) {
+		spec, err := from(ctx, reader)
+		if err != nil {
+			return nil, err
+		}
+
+		data, err := Marshal(mediaType, spec)
+		if err != nil {
+			return nil, err
+		}
+
+		return bytes.NewBuffer(data), nil
 	}
 }
