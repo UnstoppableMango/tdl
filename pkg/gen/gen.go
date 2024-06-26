bf60dd233ab84c3590f187dd84ac04bb2cc7f54a
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-26 04:03:03 -0500

    Refactor generic generator interface and functions
    The generic generator interface has been refactored for better clarity and flexibility. The previous single function interface has been replaced with a new Generator interface that includes separate input() and output() methods. Additionally, two mapping functions have been added to allow transformations on both the input and output of a Generator. A Run function is also introduced to execute the generator with given input and output.

--- pkg/gen/gen.go
+++ pkg/gen/gen.go
@@ -2,6 +2,42 @@ package gen
 
 import "context"
 
-type Gen[T, V any] interface {
-	func(context.Context, T, V) error
+type Generator[Input, Output any] interface {
+	input() Input
+	output() Output
+}
+
+type generator[I, O any] struct {
+	Input  I
+	Output O
+}
+
+// input implements Generator.
+func (g generator[I, O]) input() I {
+	return g.Input
+}
+
+// output implements Generator.
+func (g generator[I, O]) output() O {
+	return g.Output
+}
+
+var _ Generator[string, string] = generator[string, string]{}
+
+func MapI[A, B, Output any](x Generator[A, Output], f func(A) B) Generator[B, Output] {
+	return generator[B, Output]{
+		Input:  f(x.input()),
+		Output: x.output(),
+	}
+}
+
+func MapO[A, B, Input any](x Generator[Input, A], f func(A) B) Generator[Input, B] {
+	return generator[Input, B]{
+		Input:  x.input(),
+		Output: f(x.output()),
+	}
+}
+
+func Run[I, O any](ctx context.Context, g Generator[I, O], input I, output O) error {
+
 }
