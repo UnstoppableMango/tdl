976dab279f1245a05f591261b1421ed9ce333c98
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-26 22:50:58 -0500

    Refactor 'From' interface to 'Converter' struct
    The previous 'From' interface has been refactored into a new structure called 'Converter'. This change includes the introduction of a new function type, 'ConverterFunc', and two methods: a constructor method for creating new instances of the Converter struct, and a Convert method that uses the provided ConverterFunc.

--- pkg/from/from.go
+++ pkg/from/from.go
@@ -6,6 +6,16 @@ import (
 	"github.com/unstoppablemango/tdl/pkg/result"
 )
 
-type From[T, V any] interface {
-	func(context.Context, T) result.R[V]
+type ConverterFunc[I, O any] func(context.Context, I) result.R[O]
+
+type Converter[I, O any] struct {
+	run ConverterFunc[I, O]
+}
+
+func New[I, O any](c ConverterFunc[I, O]) Converter[I, O] {
+	return Converter[I, O]{run: c}
+}
+
+func (c Converter[I, O]) Convert(ctx context.Context, input I) result.R[O] {
+	return c.run(ctx, input)
 }
