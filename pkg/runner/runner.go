177a1807964d5d61b5416dbd7a77aab43d2a23cf
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-27 13:54:27 -0500

    Refactoring everything

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -1,11 +1,36 @@
 package runner
 
 import (
+	"context"
+
 	"github.com/unstoppablemango/tdl/pkg/from"
 	"github.com/unstoppablemango/tdl/pkg/gen"
+	"github.com/unstoppablemango/tdl/pkg/result"
 )
 
 type Runner[Spec, I, O any] interface {
-	gen.Generator[Spec, I]
-	from.Converter[O, Spec]
+	from.Converter[I, Spec]
+	gen.Generator[Spec, O]
+}
+
+type runner[Spec, I, O any] struct {
+	Converter from.Converter[I, Spec]
+	Generator gen.Generator[Spec, O]
+}
+
+// From implements Runner.
+func (r runner[Spec, I, O]) From(ctx context.Context, input I) result.R[Spec] {
+	return r.Converter.From(ctx, input)
+}
+
+// Gen implements Runner.
+func (r runner[Spec, I, O]) Gen(ctx context.Context, spec Spec, output O) error {
+	return r.Generator.Gen(ctx, spec, output)
+}
+
+func New[Spec, I, O any](f from.ConverterFunc[I, Spec], g gen.GeneratorFunc[Spec, O]) Runner[Spec, I, O] {
+	return runner[Spec, I, O]{
+		Converter: from.New(f),
+		Generator: gen.New(g),
+	}
 }
