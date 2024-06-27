d1f37a4d3012f09d896aee81d1703c8a3e653729
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-26 21:51:59 -0500

    Refactor gen_test.go for string output
    The test file gen_test.go has been refactored to handle string outputs instead of integer. The GeneratorFunc now takes two strings as input and returns an error. A new mapper function is introduced to convert io.Reader into a result.R[string]. Tests have been updated accordingly for MapI and MapO functions, ensuring they work as expected with the new changes.

--- pkg/gen/gen_test.go
+++ pkg/gen/gen_test.go
@@ -15,12 +15,21 @@ import (
 
 var _ = Describe("Gen", func() {
 	var input *string = nil
-	var output *int = nil
+	var output *string = nil
 
-	var sut gen.GeneratorFunc[string, int] = func(ctx context.Context, s string, i int) error {
+	var sut gen.GeneratorFunc[string, string] = func(ctx context.Context, s string, i string) error {
 		input = &s
 		output = &i
 		return nil
+	}
+
+	mapper := func(reader io.Reader) result.R[string] {
+		buf := new(strings.Builder)
+		if _, err := io.Copy(buf, reader); err != nil {
+			return result.OfErr[string](err)
+		}
+
+		return result.Of(buf.String())
 	}
 
 	BeforeEach(func() {
@@ -29,27 +38,36 @@ var _ = Describe("Gen", func() {
 	})
 
 	Describe("MapI", func() {
-		It("should work", func() {
-			expected := "blah blah blah"
+		It("should work", func(ctx context.Context) {
+			expectedInput := "blah blah blah"
+			expectedOutput := "eh doesn't matter"
+			reader := bytes.NewBufferString(expectedInput)
 
-			mapped := gen.MapI(sut, func(reader io.Reader) result.R[string] {
-				buf := new(strings.Builder)
-				if _, err := io.Copy(buf, reader); err != nil {
-					return result.OfErr[string](err)
-				}
-
-				return result.Of(buf.String())
-			})
-
-			buf := bytes.NewBufferString(expected)
-
-			err := mapped(context.TODO(), buf, 69)
+			mapped := gen.MapI(sut, mapper)
+			err := mapped(ctx, reader, expectedOutput)
 
 			Expect(err).NotTo(HaveOccurred())
 			Expect(input).NotTo(BeNil())
-			Expect(*input).To(Equal(expected))
+			Expect(*input).To(Equal(expectedInput))
 			Expect(output).NotTo(BeNil())
-			Expect(*output).To(Equal(69))
+			Expect(*output).To(Equal(expectedOutput))
+		})
+	})
+
+	Describe("MapO", func() {
+		It("should work", func(ctx context.Context) {
+			expectedInput := "blah blah blah"
+			expectedOutput := "eh doesn't matter"
+			reader := bytes.NewBufferString(expectedOutput)
+
+			mapped := gen.MapO(sut, mapper)
+			err := mapped(ctx, expectedInput, reader)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(input).NotTo(BeNil())
+			Expect(*input).To(Equal(expectedInput))
+			Expect(output).NotTo(BeNil())
+			Expect(*output).To(Equal(expectedOutput))
 		})
 	})
 })
