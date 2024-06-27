f4d3b84c45df3334639c247f70587d7c91c25599
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-26 22:31:38 -0500

    Added Generator struct and methods
    A new struct, Generator, has been introduced along with its associated methods. This includes a constructor function for creating new instances of the Generator type and a Generate method that uses the provided generator function to produce output from input. The changes enhance flexibility by allowing different generator functions to be used interchangeably.

--- pkg/gen/gen.go
+++ pkg/gen/gen.go
@@ -5,3 +5,15 @@ import (
 )
 
 type GeneratorFunc[I, O any] func(context.Context, I, O) error
+
+type Generator[I, O any] struct {
+	run GeneratorFunc[I, O]
+}
+
+func New[I, O any](g GeneratorFunc[I, O]) Generator[I, O] {
+	return Generator[I, O]{run: g}
+}
+
+func (g Generator[I, O]) Generate(ctx context.Context, input I, output O) error {
+	return g.run(ctx, input, output)
+}
