85257f81a1c520dfa1be935e4e70edf6b48dc00b
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-26 22:31:51 -0500

    Added echo runner in runner package
    A new file 'echo.go' has been added to the 'runner' package. This includes the implementation of an echo generator and an echo runner. The echo generator uses protobuf to marshal a UML specification into data, which is then written using a writer. The echo runner currently has unimplemented 'From' method and its 'Gen' method utilizes the aforementioned echo generator for generating output based on UML specifications.

---
+++ pkg/runner/echo.go
@@ -0,0 +1,36 @@
+package runner
+
+import (
+	"context"
+	"io"
+
+	"github.com/unstoppablemango/tdl/pkg/gen"
+	"github.com/unstoppablemango/tdl/pkg/uml"
+	"google.golang.org/protobuf/proto"
+)
+
+var (
+	echoGenerator = gen.New(func(ctx context.Context, spec *uml.Spec, writer io.Writer) error {
+		data, err := proto.Marshal(spec)
+		if err != nil {
+			return err
+		}
+
+		_, err = writer.Write(data)
+		return err
+	})
+)
+
+type echoRunner struct{}
+
+// From implements uml.Runner.
+func (e echoRunner) From(ctx context.Context, reader io.Reader) (*uml.Spec, error) {
+	panic("unimplemented")
+}
+
+// Gen implements uml.Runner.
+func (e echoRunner) Gen(ctx context.Context, spec *uml.Spec, writer io.Writer) error {
+	return echoGenerator.Generate(ctx, spec, writer)
+}
+
+var _ uml.Runner = echoRunner{}
