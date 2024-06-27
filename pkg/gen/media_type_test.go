236325812d1082834a6fd16a6d52ac59fa2eb108
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-26 22:44:54 -0500

    Added echo runner in media type test
    In the media type test, a new instance of the echo runner has been introduced. This addition is expected to enhance testing capabilities by providing more control over the execution environment.

--- pkg/gen/media_type_test.go
+++ pkg/gen/media_type_test.go
@@ -3,10 +3,11 @@ package gen_test
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/unstoppablemango/tdl/pkg/runner"
 )
 
 var _ = Describe("MediaType", func() {
 	Describe("FromMediaType", func() {
-
+		echo := runner.NewEcho()
 	})
 })
