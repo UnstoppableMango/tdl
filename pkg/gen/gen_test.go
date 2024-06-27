feb1b03e0e23da624b66caefdf18bb5f32038b31
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-26 20:52:08 -0500

    Refactored test suite to use Ginkgo and Gomega
    The testing framework has been switched from the standard library's testing package to Ginkgo and Gomega. This change includes refactoring of existing tests, addition of new ones, and introduction of setup routines for each test. The GeneratorFunc has also been modified to capture input and output for verification in tests.

--- pkg/gen/gen_test.go
+++ pkg/gen/gen_test.go
@@ -1,27 +1,55 @@
-package gen
+package gen_test
 
 import (
+	"bytes"
 	"context"
 	"io"
-	"testing"
+	"strings"
 
-	"github.com/unstoppablemango/tdl/pkg/uml"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/unstoppablemango/tdl/pkg/gen"
+	"github.com/unstoppablemango/tdl/pkg/result"
 )
 
-type Test struct{}
+var _ = Describe("Gen", func() {
+	var input *string = nil
+	var output *int = nil
 
-//func TestGen(ctx context.Context, spec string, writer io.Writer) error {
-//	return errors.New("TODO")
-//}
-
-func MapTest(t *testing.T) {
-	gen := New(func(ctx context.Context, s *uml.Spec, w io.Writer) error {
+	var sut gen.GeneratorFunc[string, int] = func(ctx context.Context, s string, i int) error {
+		input = &s
+		output = &i
 		return nil
+	}
+
+	BeforeEach(func() {
+		input = nil
+		output = nil
 	})
 
-	actual := MapI(gen, func(reader io.Reader) *uml.Spec {
-		return nil
-	})
+	Describe("MapI", func() {
+		It("should work", func() {
+			expected := "blah blah blah"
 
-	Run(context.TODO(), actual)
-}
+			mapped := gen.MapI(sut, func(reader io.Reader) result.R[string] {
+				buf := new(strings.Builder)
+				if _, err := io.Copy(buf, reader); err != nil {
+					return result.OfErr[string](err)
+				}
+
+				return result.Of(buf.String())
+			})
+
+			buf := bytes.NewBufferString(expected)
+
+			err := mapped(context.TODO(), buf, 69)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(input).NotTo(BeNil())
+			Expect(*input).To(Equal(expected))
+			Expect(output).NotTo(BeNil())
+			Expect(*output).To(Equal(69))
+		})
+	})
+})
