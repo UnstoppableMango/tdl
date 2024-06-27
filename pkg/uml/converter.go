bc72645c1c32b2d16acac68dd3cfdf1f55a0f74f
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-26 22:13:26 -0500

    Refactor UML converter code
    The UML converter code has been significantly refactored. The unused "bytes" and "result" packages have been removed from the import statements. The generic interface 'From' and its related functions 'MapFromInput', 'MapFromOutput', and 'FromMediaType' have also been deleted as they were not being used in the current context. This simplifies the overall structure of the code, making it easier to maintain and understand.

--- pkg/uml/converter.go
+++ pkg/uml/converter.go
@@ -1,12 +1,9 @@
 package uml
 
 import (
-	"bytes"
 	"context"
 	"io"
 	"log/slog"
-
-	"github.com/unstoppablemango/tdl/pkg/result"
 )
 
 type ConverterOptions struct {
@@ -16,10 +13,6 @@ type ConverterOptions struct {
 }
 
 type ConverterOption func(*ConverterOptions) error
-
-type From[T, V any] interface {
-	func(context.Context, T) result.R[V]
-}
 
 type Converter interface {
 	From(ctx context.Context, reader io.Reader) (*Spec, error)
@@ -33,33 +26,5 @@ func WithMediaType(t string) ConverterOption {
 	return func(opts *ConverterOptions) error {
 		opts.MediaType = &t
 		return nil
-	}
-}
-
-func MapFromInput[A, B, C any, F From[A, C], R From[B, C]](from F, f func(B) A) R {
-	return func(ctx context.Context, input B) result.R[C] {
-		return from(ctx, f(input))
-	}
-}
-
-func MapFromOutput[A, B, C any, F From[A, B], R From[A, C]](from F, f func(B) C) R {
-	return func(ctx context.Context, input A) result.R[C] {
-		return result.Map(from(ctx, input), f)
-	}
-}
-
-func FromMediaType[F From[io.Reader, *Spec], R From[io.Reader, io.Reader]](mediaType string, from F) R {
-	return func(ctx context.Context, reader io.Reader) result.R[io.Reader] {
-		spec, err := from(ctx, reader)
-		if err != nil {
-			return nil, err
-		}
-
-		data, err := Marshal(mediaType, spec)
-		if err != nil {
-			return nil, err
-		}
-
-		return bytes.NewBuffer(data), nil
 	}
 }
