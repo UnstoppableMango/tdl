ea42ca814cd4b2809387aa6552e0102068db461b
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-06-27 21:19:40 -0500

    Enhanced writer package with monoid concept
    The writer package has been significantly expanded. The Writer interface now includes a new type, 'monoid', which provides methods for returning an empty value and appending two values of the same type. Two concrete implementations of this monoid concept have been added: 'sliceMonoid' for slices and 'intMonoid' for integers. This enhancement allows more flexibility in handling different data types within the writer package.

--- pkg/writer/writer.go
+++ pkg/writer/writer.go
@@ -1,3 +1,30 @@
 package writer
 
-type Writer[T any] interface{}
+type monoid[T any] interface {
+	Empty() T
+	Append(T, T) T
+}
+
+type Writer[T any, V monoid[T]] interface{}
+
+type sliceMonoid[T any] struct{}
+
+func (m sliceMonoid[T]) Empty() []T {
+	return []T{}
+}
+
+func (m sliceMonoid[T]) Append(a []T, b []T) []T {
+	return append(a, b...)
+}
+
+type intMonoid struct{}
+
+func (m intMonoid) Empty() int {
+	return 0
+}
+
+func (m intMonoid) Append(a int, b int) int {
+	return a + b
+}
+
+var _ Writer[int, intMonoid] = intMonoid{}
