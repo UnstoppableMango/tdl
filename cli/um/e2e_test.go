29181b67c754fe34dd3b19d985febf6b02b509f0
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-07-22 12:26:24 -0500

    Refactor suite again using subtree

--- cli/um/e2e_test.go
+++ cli/um/e2e_test.go
@@ -3,13 +3,10 @@ package main_test
 import (
 	"bytes"
 	"embed"
-	"fmt"
-	"io"
 	"io/fs"
 	"os"
 	"os/exec"
 	"path"
-	"regexp"
 	"strings"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -23,37 +20,33 @@ import (
 var testdata embed.FS
 
 var (
-	matcher = regexp.MustCompile(`(?P<name>\w*)\..*`)
-	binDir  = os.Getenv("BIN_DIR")
+	binDir = os.Getenv("BIN_DIR")
 )
 
-type Test struct {
-	Name   string
-	Source io.Reader
-	Target io.Reader
-}
+var _ = DescribeTableSubtree("End to end",
+	Entry("interface", "interface", "source.yml", "target.ts"),
+	Entry("nested_interface", "nested_interface", "source.yml", "target.ts"),
+	func(dir, sourceFile, targetFile string) {
+		var source, target []byte
 
-var _ = Describe("End to end", func() {
-	BeforeSuite(func() {
-		if binDir == "" {
-			Skip("BIN_DIR not found")
-		}
-	})
+		BeforeEach(func() {
+			if binDir == "" {
+				Skip("BIN_DIR not found")
+			}
 
-	tests, err := readTests()
-	if err != nil {
-		Fail(err.Error())
-	}
-
-	for _, test := range tests {
-		It(fmt.Sprintf("should generate %s", test.Name), func() {
-			By("Reading source")
-			source, err := io.ReadAll(test.Source)
+			data, err := fs.ReadFile(testdata, path.Join("testdata", dir, sourceFile))
 			Expect(err).NotTo(HaveOccurred())
+			source = data
 
+			data, err = fs.ReadFile(testdata, path.Join("testdata", dir, targetFile))
+			Expect(err).NotTo(HaveOccurred())
+			target = data
+		})
+
+		It("should generate", func() {
 			By("Unmarshaling source")
 			spec := &uml.Spec{}
-			err = yaml.Unmarshal(source, spec)
+			err := yaml.Unmarshal(source, spec)
 			Expect(err).NotTo(HaveOccurred())
 
 			stdout := &bytes.Buffer{}
@@ -71,53 +64,9 @@ var _ = Describe("End to end", func() {
 			err = cmd.Run()
 			Expect(err).NotTo(HaveOccurred())
 
-			By("Reading target")
-			expectedBytes, err := io.ReadAll(test.Target)
-			Expect(err).NotTo(HaveOccurred())
-
-			expected := strings.TrimSpace(string(expectedBytes))
+			expected := strings.TrimSpace(string(target))
 			actual := strings.TrimSpace(stdout.String())
 			Expect(actual).To(Equal(expected))
 		})
-	}
-})
-
-func readTests() ([]Test, error) {
-	tests := []Test{}
-	builder := NewTestBuilder()
-	err := fs.WalkDir(testdata, "testdata", func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-
-		switch strings.Count(path, "/") {
-		case 1:
-			builder.WithName(d.Name())
-		case 2:
-			file := d.Name()
-			matches := matcher.FindStringSubmatch(file)
-			i := matcher.SubexpIndex("name")
-
-			reader, err := os.Open(path)
-			if err != nil {
-				return err
-			}
-
-			switch matches[i] {
-			case "source":
-				builder.WithSource(reader)
-			case "target":
-				builder.WithTarget(reader)
-			}
-		}
-
-		if test, done := builder.Done(); done {
-			tests = append(tests, test)
-			builder = NewTestBuilder()
-		}
-
-		return nil
-	})
-
-	return tests, err
-}
+	},
+)
