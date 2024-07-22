7909516986688c6208203b33b7ad1220efe7f796
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-07-22 17:08:17 -0500

    Refactoring root cmd

--- cli/um/cmd/root.go
+++ cli/um/cmd/root.go
@@ -2,19 +2,25 @@ package cmd
 
 import (
 	"os"
+	"path"
 
+	"github.com/adrg/xdg"
 	"github.com/spf13/cobra"
 	cli "github.com/unstoppablemango/tdl/cli/internal"
+	"github.com/unstoppablemango/tdl/pkg/logging"
+)
+
+var (
+	AppName   = "um"
+	ConfigDir = path.Join(xdg.ConfigHome, AppName)
+	PluginDir = path.Join(ConfigDir, "plugins")
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "um",
 	Short: "UnstoppableMango's Type Description Language CLI",
 	PreRun: func(cmd *cobra.Command, args []string) {
-		cmd.SetContext(cli.WithLogger(
-			cmd.Context(),
-			cli.NewLogger(),
-		))
+		cli.SetLogger(cmd, logging.NewLogger())
 	},
 }
 
