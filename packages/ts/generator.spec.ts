285ae51d37fd7327ffd2415d3cc0a97ccce785a9
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-07-24 22:32:11 -0500

    Add some tests for determinism (#290)
    * Bun lock I guess
    
    * Go cache paths
    
    * Bun stuff
    
    * More tests that really arent helping as it happens
    
    * That also did not help as much as I had hoped
    
    * Format

--- packages/ts/generator.spec.ts
+++ packages/ts/generator.spec.ts
@@ -1,9 +1,26 @@
 import { Spec } from '@unmango/tdl-es';
 import { ArrayBufferSink } from 'bun';
 import { describe, expect, it } from 'bun:test';
+import fc from 'fast-check';
 import * as YAML from 'yaml';
 import { gen } from './generator';
 import { tests } from './testdata';
+
+const arbSpec = () =>
+	fc.gen().map(g =>
+		new Spec({
+			name: g(fc.string),
+			types: {
+				test: {
+					fields: {
+						a: { type: 'string' },
+						b: { type: 'boolean' },
+						c: { type: 'number' },
+					},
+				},
+			},
+		})
+	);
 
 describe('gen', () => {
 	it.each(tests)('should generate %p', async (_, source, target) => {
@@ -17,5 +34,25 @@ describe('gen', () => {
 
 		expect(actual).not.toBeNull();
 		expect(actual).toEqual(target);
+	});
+
+	it('should be deterministic', async () => {
+		await fc.assert(
+			fc.asyncProperty(arbSpec(), async (spec) => {
+				const bufA = new ArrayBufferSink();
+				await gen(spec, bufA);
+				const decoder = new TextDecoder();
+				const a = decoder.decode(bufA.end());
+
+				const bufB = new ArrayBufferSink();
+				await gen(spec, bufB);
+				const b = decoder.decode(bufB.end());
+
+				expect(a).not.toBeNull();
+				expect(b).not.toBeNull();
+				expect(b).toEqual(a);
+			}),
+			{ numRuns: 1_000 },
+		);
 	});
 });
