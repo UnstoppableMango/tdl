3013a038a882720c048ab1d8f335c0b67172a39f
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-08-24 13:54:55 -0500

    Fix linting errors
    AND SOMEHOW STILL MORE GO WORK STUFF

--- pkg/result/iter.go
+++ pkg/result/iter.go
@@ -8,3 +8,15 @@ func Iter[T any](r R[T], f func(T)) error {
 
 	return err
 }
+
+// I can't remember what this function is supposed to be called but I need
+// this functionality so it's gonna be called IterE until I decide to fix it
+
+func IterE[T any](r R[T], f func(T) error) error {
+	x, err := r.Unwrap()
+	if err == nil {
+		return f(x)
+	}
+
+	return err
+}
