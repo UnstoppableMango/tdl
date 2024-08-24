516ecfcc6b36a5a5121f7686f01b6e41f54d0532
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-08-24 14:00:10 -0500

    Even more go crap (#329)
    * Upgrading transitive dependencies
    
    * Upgrade go to 1.23
    
    * Fix linting errors
    
    AND SOMEHOW STILL MORE GO WORK STUFF
    
    * Update golangci-lint version in GitHub Actions
    
    The golangci-lint version used in the GitHub Actions workflow has been updated. The previous version was v1.57 and it has now been upgraded to v1.60.3 for improved linting capabilities and bug fixes.

--- pkg/gen/map.go
+++ pkg/gen/map.go
@@ -8,16 +8,16 @@ import (
 
 func MapI[A, B, Output any](x GeneratorFunc[A, Output], f func(B) result.R[A]) GeneratorFunc[B, Output] {
 	return func(ctx context.Context, b B, output Output) error {
-		return result.Iter(f(b), func(a A) {
-			x.Gen(ctx, a, output)
+		return result.IterE(f(b), func(a A) error {
+			return x.Gen(ctx, a, output)
 		})
 	}
 }
 
 func MapO[A, B, Input any](x GeneratorFunc[Input, A], f func(B) result.R[A]) GeneratorFunc[Input, B] {
 	return func(ctx context.Context, input Input, b B) error {
-		return result.Iter(f(b), func(a A) {
-			x.Gen(ctx, input, a)
+		return result.IterE(f(b), func(a A) error {
+			return x.Gen(ctx, input, a)
 		})
 	}
 }
