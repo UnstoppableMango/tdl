6358739346d0f90c7670caf5345aa6b232d62ea2
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-10-27 11:43:54 -0500

    WTF it worked

--- cmd/ux/e2e_test.go
+++ cmd/ux/e2e_test.go
@@ -1,14 +1,19 @@
 package main_test
 
 import (
+	"bytes"
 	"context"
 	"errors"
+	"fmt"
 	"io"
 	"os/exec"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/unstoppablemango/tdl/pkg/conform"
+	tdlv1alpha1 "github.com/unstoppablemango/tdl/pkg/unmango/dev/tdl/v1alpha1"
+	"google.golang.org/protobuf/proto"
+	"gopkg.in/yaml.v3"
 )
 
 var _ = Describe("End to end", func() {
@@ -35,9 +40,21 @@ func ExecuteIO(input io.Reader, output io.Writer) error {
 	if bin == "" {
 		return errors.New("test has not been initialized: bin was empty")
 	}
+	data, err := io.ReadAll(input)
+	if err != nil {
+		return fmt.Errorf("reading input: %w", err)
+	}
+	var spec tdlv1alpha1.Spec
+	if err := yaml.Unmarshal(data, &spec); err != nil {
+		return fmt.Errorf("reading spec: %w", err)
+	}
+	protoInput, err := proto.Marshal(&spec)
+	if err != nil {
+		return fmt.Errorf("marshalling spec: %w", err)
+	}
 
 	cmd := exec.Command(bin, "gen", "ts")
-	cmd.Stdin = input
+	cmd.Stdin = bytes.NewReader(protoInput)
 	cmd.Stdout = output
 	cmd.Stderr = output
 
