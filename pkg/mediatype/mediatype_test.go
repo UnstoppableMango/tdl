a1338694f790a956aff448fdf3d0063d418f03a0
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-11-02 18:37:46 -0500

    Simplified test descriptions in mediatype tests
    Removed the use of fmt.Sprintf for generating test descriptions in the mediatype package. The descriptions are now nil, focusing on testing the functionality rather than creating descriptive strings. This simplifies the code and makes it easier to read and maintain.

--- pkg/mediatype/mediatype_test.go
+++ pkg/mediatype/mediatype_test.go
@@ -1,8 +1,6 @@
 package mediatype_test
 
 import (
-	"fmt"
-
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
@@ -12,28 +10,28 @@ import (
 
 var _ = Describe("Mediatype", func() {
 	DescribeTable("Parse",
-		Entry(fmt.Sprintf("should parse %s", mediatype.ApplicationGoogleProtobuf), "application/vnd.google.protobuf"),
-		Entry(fmt.Sprintf("should parse %s", mediatype.ApplicationJson), "application/json"),
-		Entry(fmt.Sprintf("should parse %s", mediatype.ApplicationProtobuf), "application/protobuf"),
-		Entry(fmt.Sprintf("should parse %s", mediatype.ApplicationXProtobuf), "application/x-protobuf"),
-		Entry(fmt.Sprintf("should parse %s", mediatype.ApplicationXYaml), "application/x-yaml"),
-		Entry(fmt.Sprintf("should parse %s", mediatype.ApplicationYaml), "application/yaml"),
-		Entry(fmt.Sprintf("should parse %s", mediatype.TextJson), "text/json"),
-		Entry(fmt.Sprintf("should parse %s", mediatype.TextYaml), "text/yaml"),
+		Entry(nil, "application/vnd.google.protobuf"),
+		Entry(nil, "application/json"),
+		Entry(nil, "application/protobuf"),
+		Entry(nil, "application/x-protobuf"),
+		Entry(nil, "application/x-yaml"),
+		Entry(nil, "application/yaml"),
+		Entry(nil, "text/json"),
+		Entry(nil, "text/yaml"),
 		func(value string) {
 			Expect(mediatype.Parse(value)).NotTo(BeEmpty())
 		},
 	)
 
 	DescribeTable("Parse unsupported",
-		Entry(fmt.Sprintf("should not parse %s", "vnd.google.protobuf"), "vnd.google.protobuf"),
-		Entry(fmt.Sprintf("should not parse %s", "application/jsno"), "application/jsno"),
-		Entry(fmt.Sprintf("should not parse %s", "aplication/protobuf"), "aplication/protobuf"),
-		Entry(fmt.Sprintf("should not parse %s", "application/xprotobuf"), "application/xprotobuf"),
-		Entry(fmt.Sprintf("should not parse %s", "application/x-yml"), "application/x-yml"),
-		Entry(fmt.Sprintf("should not parse %s", "application/yml"), "application/yml"),
-		Entry(fmt.Sprintf("should not parse %s", "txt/json"), "txt/json"),
-		Entry(fmt.Sprintf("should not parse %s", "textyaml"), "textyaml"),
+		Entry(nil, "vnd.google.protobuf"),
+		Entry(nil, "application/jsno"),
+		Entry(nil, "aplication/protobuf"),
+		Entry(nil, "application/xprotobuf"),
+		Entry(nil, "application/x-yml"),
+		Entry(nil, "application/yml"),
+		Entry(nil, "txt/json"),
+		Entry(nil, "textyaml"),
 		func(value string) {
 			_, err := mediatype.Parse(value)
 
@@ -42,14 +40,14 @@ var _ = Describe("Mediatype", func() {
 	)
 
 	DescribeTable("Supported",
-		Entry(fmt.Sprintf("should support %s", mediatype.ApplicationGoogleProtobuf), mediatype.ApplicationGoogleProtobuf),
-		Entry(fmt.Sprintf("should support %s", mediatype.ApplicationJson), mediatype.ApplicationJson),
-		Entry(fmt.Sprintf("should support %s", mediatype.ApplicationProtobuf), mediatype.ApplicationProtobuf),
-		Entry(fmt.Sprintf("should support %s", mediatype.ApplicationXProtobuf), mediatype.ApplicationXProtobuf),
-		Entry(fmt.Sprintf("should support %s", mediatype.ApplicationXYaml), mediatype.ApplicationXYaml),
-		Entry(fmt.Sprintf("should support %s", mediatype.ApplicationYaml), mediatype.ApplicationYaml),
-		Entry(fmt.Sprintf("should support %s", mediatype.TextJson), mediatype.TextJson),
-		Entry(fmt.Sprintf("should support %s", mediatype.TextYaml), mediatype.TextYaml),
+		Entry(nil, mediatype.ApplicationGoogleProtobuf),
+		Entry(nil, mediatype.ApplicationJson),
+		Entry(nil, mediatype.ApplicationProtobuf),
+		Entry(nil, mediatype.ApplicationXProtobuf),
+		Entry(nil, mediatype.ApplicationXYaml),
+		Entry(nil, mediatype.ApplicationYaml),
+		Entry(nil, mediatype.TextJson),
+		Entry(nil, mediatype.TextYaml),
 		func(value tdl.MediaType) {
 			Expect(mediatype.Supported(value)).To(BeTrue())
 		},
