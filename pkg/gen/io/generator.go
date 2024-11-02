6136dce5135f64f4f782e9198a9280520c2ac350
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-11-02 17:52:11 -0500

    It still compiles

--- pkg/gen/io/generator.go
+++ pkg/gen/io/generator.go
@@ -6,21 +6,33 @@ import (
 	"github.com/unstoppablemango/tdl/pkg/pipe"
 	"github.com/unstoppablemango/tdl/pkg/tdl"
 	"github.com/unstoppablemango/tdl/pkg/tdl/constraint"
+	"github.com/unstoppablemango/tdl/pkg/tdl/spec"
 	tdlv1alpha1 "github.com/unstoppablemango/tdl/pkg/unmango/dev/tdl/v1alpha1"
 )
 
+type reader[T any] constraint.Pipeline[*tdlv1alpha1.Spec, T]
+
 type SpecReader[T any] struct {
-	gen tdl.Pipeline[*tdlv1alpha1.Spec, T]
+	tdl.Pipeline[*tdlv1alpha1.Spec, T]
+	options []spec.ReaderOption
 }
 
 // Execute implements tdl.Pipeline.
-func (p *SpecReader[T]) Execute(reader io.Reader, sink tdl.Sink) error {
-	panic("unimplemented")
+func (p *SpecReader[T]) Execute(reader io.Reader, sink T) error {
+	spec, err := spec.ReadAll(reader)
+	if err != nil {
+		return err
+	}
+
+	return p.Pipeline.Execute(spec, sink)
 }
 
-func WithSpecFrom[
-	T any,
-	G constraint.Pipeline[*tdlv1alpha1.Spec, T],
-](generator G) tdl.Pipeline[io.Reader, tdl.Sink] {
-	return &SpecReader[T]{gen: pipe.Lift(generator)}
+func ReadSpec[T any, P reader[T]](
+	pipeline P,
+	options ...spec.ReaderOption,
+) tdl.Pipeline[io.Reader, T] {
+	return &SpecReader[T]{
+		Pipeline: pipe.Lift(pipeline),
+		options:  options,
+	}
 }
--- pkg/tdl/constraint/constraint.go
+++ pkg/tdl/constraint/constraint.go
@@ -1,13 +1,19 @@
 package constraint
 
 import (
+	"io"
+
 	"github.com/unstoppablemango/tdl/pkg/tdl"
 	tdlv1alpha1 "github.com/unstoppablemango/tdl/pkg/unmango/dev/tdl/v1alpha1"
 )
 
-type Gen interface {
-	~func(*tdlv1alpha1.Spec, tdl.Sink) error
-}
+type (
+	FromReader[T any] Pipeline[io.Reader, T]
+	ToWriter[T any]   Pipeline[T, io.Writer]
+	SpecReader[T any] Pipeline[*tdlv1alpha1.Spec, T]
+)
+
+type Gen Pipeline[*tdlv1alpha1.Spec, tdl.Sink]
 
 type Pipeline[T, V any] interface {
 	~func(T, V) error
