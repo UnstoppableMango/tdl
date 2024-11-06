bcf629f096bde3965ef0f5c462650dd51d735a6b
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-11-06 11:11:31 -0600

    More unwrap tests

--- pkg/plugin/unwrap.go
+++ pkg/plugin/unwrap.go
@@ -1,46 +1,33 @@
 package plugin
 
 import (
-	"github.com/charmbracelet/log"
 	tdl "github.com/unstoppablemango/tdl/pkg"
 )
+
+// This is probably really inefficient, but we'll get there eventually
+// TODO: Ordering?
 
 func Unwrap(plugin tdl.Plugin) []tdl.Plugin {
 	return unwrapRec(plugin, 0)
 }
 
 func UnwrapAll(plugins []tdl.Plugin) []tdl.Plugin {
-	res := []tdl.Plugin{}
-	for _, p := range plugins {
-		res = append(res, Unwrap(p)...)
-	}
-
-	return res
+	return unwrapRec(NewAggregate(plugins...), -1)
 }
 
-// This is probably really inefficient, but we'll get there eventually
-// TODO: Ordering?
-
 func unwrapRec(plugin tdl.Plugin, depth int) []tdl.Plugin {
-	log := log.With("plugin", plugin, "depth", depth)
-
 	plugins, isAggregate := plugin.(Aggregate)
 	if !isAggregate {
-		log.Debug("not an aggregate")
 		return []tdl.Plugin{plugin}
 	}
-
 	if depth >= UnwrapDepth {
-		log.Debug("at depth")
 		return plugins
 	}
 
 	acc := []tdl.Plugin{}
 	for _, p := range plugins {
-		log.Debug("unwrapping", "inner", p)
 		acc = append(acc, unwrapRec(p, depth+1)...)
 	}
 
-	log.Debug("returning accumulated plugins", "accumulator", acc)
 	return acc
 }
--- pkg/plugin/unwrap_test.go
+++ pkg/plugin/unwrap_test.go
@@ -4,11 +4,20 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
+	tdl "github.com/unstoppablemango/tdl/pkg"
 	"github.com/unstoppablemango/tdl/pkg/plugin"
 	"github.com/unstoppablemango/tdl/pkg/testing"
 )
 
 var _ = Describe("Unwrap", func() {
+	It("should ignore non-aggregate plugin", func() {
+		p := testing.NewMockPlugin()
+
+		r := plugin.Unwrap(p)
+
+		Expect(r).To(ConsistOf(p))
+	})
+
 	It("should unwrap an aggregate plugin", func() {
 		p := testing.NewMockPlugin()
 		agg := plugin.NewAggregate(p)
@@ -18,22 +27,12 @@ var _ = Describe("Unwrap", func() {
 		Expect(r).To(ConsistOf(p))
 	})
 
-	It("should unwarp multiple aggregate plugins", func() {
+	It("should unwrap multiple aggregate plugins", func() {
 		p1 := testing.NewMockPlugin()
 		p2 := testing.NewMockPlugin()
 		agg := plugin.NewAggregate(p1, p2)
 
 		r := plugin.Unwrap(agg)
-
-		Expect(r).To(ConsistOf(p1, p2))
-	})
-
-	It("should unwarp all aggregate plugins", func() {
-		p1 := testing.NewMockPlugin()
-		p2 := testing.NewMockPlugin()
-		agg := plugin.NewAggregate(p1, p2)
-
-		r := plugin.UnwrapAll(agg)
 
 		Expect(r).To(ConsistOf(p1, p2))
 	})
@@ -70,8 +69,51 @@ var _ = Describe("Unwrap", func() {
 
 		r := plugin.Unwrap(agg)
 
-		Expect(r).To(ConsistOf(
-			plugin.NewAggregate(p),
-		))
+		Expect(r).To(ConsistOf(plugin.NewAggregate(p)))
+	})
+
+	Describe("UnwrapAll", func() {
+		It("should ignore all non-aggregate plugins", func() {
+			p1 := testing.NewMockPlugin()
+			p2 := testing.NewMockPlugin()
+
+			r := plugin.UnwrapAll([]tdl.Plugin{p1, p2})
+
+			Expect(r).To(ConsistOf(p1, p2))
+		})
+
+		It("should unwrap all aggregate plugins", func() {
+			p1 := testing.NewMockPlugin()
+			p2 := testing.NewMockPlugin()
+			agg := plugin.NewAggregate(p1, p2)
+
+			r := plugin.UnwrapAll(agg)
+
+			Expect(r).To(ConsistOf(p1, p2))
+		})
+
+		It("should unwrap all nested aggregate plugins up to max depth", func() {
+			p := testing.NewMockPlugin()
+			agg := plugin.NewAggregate(p)
+			for i := 0; i < plugin.UnwrapDepth-1; i++ {
+				agg = plugin.NewAggregate(agg)
+			}
+
+			r := plugin.UnwrapAll(agg)
+
+			Expect(r).To(ConsistOf(p))
+		})
+
+		It("should stop unwrapping all nested aggregate plugins after max depth", func() {
+			p := testing.NewMockPlugin()
+			agg := plugin.NewAggregate(p)
+			for i := 0; i < plugin.UnwrapDepth; i++ {
+				agg = plugin.NewAggregate(agg)
+			}
+
+			r := plugin.UnwrapAll(agg)
+
+			Expect(r).To(ConsistOf(plugin.NewAggregate(p)))
+		})
 	})
 })
