2cf0abb8224b25804a1e5f311051092a4f3a7761
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-11-05 19:10:12 -0600

    WIP

--- pkg/cmd/which.go
+++ pkg/cmd/which.go
@@ -8,6 +8,7 @@ import (
 	"github.com/spf13/cobra"
 	tdl "github.com/unstoppablemango/tdl/pkg"
 	"github.com/unstoppablemango/tdl/pkg/gen"
+	"github.com/unstoppablemango/tdl/pkg/target"
 )
 
 func NewWhich() *cobra.Command {
@@ -16,18 +17,9 @@ func NewWhich() *cobra.Command {
 		Short: "Print the token for the closest matching generator",
 		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			token := tdl.Token{Path: args[0]}
-
-			generator, err := gen.Name(token)
-			if !errors.Is(err, gen.ErrNotFound) {
-				fmt.Fprintln(os.Stderr, err.Error())
-				os.Exit(1)
-			}
-
-			generator, err = gen.FromPath(token)
+			t, err := target.Parse(args[0])
 			if err != nil {
 				fmt.Fprintln(os.Stderr, err.Error())
-				os.Exit(1)
 			}
 
 			fmt.Println(generator)
