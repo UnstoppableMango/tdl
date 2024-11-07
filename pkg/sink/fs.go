2ac42bbfa471e9b1cf02d4c2e9b42d520ce01746
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-11-07 14:18:16 -0600

    FS sink and renames

---
+++ pkg/sink/fs.go
@@ -0,0 +1,50 @@
+package sink
+
+import (
+	"fmt"
+	"io"
+	"iter"
+	"slices"
+
+	"github.com/spf13/afero"
+	tdl "github.com/unstoppablemango/tdl/pkg"
+)
+
+type Fs interface {
+	Reader
+	tdl.Sink
+}
+
+type fsSink struct {
+	afero.Fs
+	files []string
+}
+
+// Reader implements Fs.
+func (f *fsSink) Reader(path string) (io.Reader, error) {
+	if !slices.Contains(f.files, path) {
+		return nil, fmt.Errorf("unknown unit: %s", path)
+	}
+
+	return f.Open(path)
+}
+
+// Units implements Fs.
+func (f *fsSink) Units() iter.Seq[string] {
+	return slices.Values(f.files)
+}
+
+// WriteUnit implements tdl.Sink.
+func (f *fsSink) WriteUnit(path string, reader io.Reader) error {
+	err := afero.WriteReader(f, path, reader)
+	if err != nil {
+		return fmt.Errorf("writing unit: %w", err)
+	}
+
+	f.files = append(f.files, path)
+	return nil
+}
+
+func NewFs(fsys afero.Fs) Fs {
+	return &fsSink{fsys, []string{}}
+}
