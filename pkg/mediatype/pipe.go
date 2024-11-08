d7d4c0f8a9a0b4fb801141f4a214bf1a8ecb7e01
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-11-07 18:32:11 -0600

    Jank

--- pkg/mediatype/pipe.go
+++ pkg/mediatype/pipe.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/spf13/afero"
 	tdl "github.com/unstoppablemango/tdl/pkg"
 	c "github.com/unstoppablemango/tdl/pkg/constraint"
 	"google.golang.org/protobuf/proto"
@@ -23,6 +24,36 @@ func PipeRead[
 		message := zero() // TODO: Will ProtoReflect().Type().Zero() work?
 		if err = Unmarshal(data, message, media); err != nil {
 			return fmt.Errorf("unmarshaling message: %w", err)
+		}
+
+		return pipeline(message, t)
+	}
+}
+
+func PipeFs[
+	I c.Pipeline[M, T],
+	O c.Pipeline[afero.Fs, T],
+	M proto.Message, T any,
+](pipeline I, path string, zero func() M) O {
+	return func(fsys afero.Fs, t T) error {
+		media, err := Guess(path)
+		if err != nil {
+			return fmt.Errorf("guessing media type: %w", err)
+		}
+
+		file, err := fsys.Open(path)
+		if err != nil {
+			return fmt.Errorf("opening input: %w", err)
+		}
+
+		data, err := io.ReadAll(file)
+		if err != nil {
+			return fmt.Errorf("reading input: %w", err)
+		}
+
+		message := zero()
+		if err = Unmarshal(data, message, media); err != nil {
+			return fmt.Errorf("reading input: %w", err)
 		}
 
 		return pipeline(message, t)
