78703247b8dd077b1f6f575d7b1d53d9b147232f
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-11-15 16:12:32 -0600

    Mediatype reader

---
+++ pkg/mediatype/reader.go
@@ -0,0 +1,49 @@
+package mediatype
+
+import (
+	"bytes"
+	"io"
+
+	tdl "github.com/unstoppablemango/tdl/pkg"
+	"google.golang.org/protobuf/proto"
+)
+
+type reader struct {
+	media   tdl.MediaType
+	message proto.Message
+
+	buf *bytes.Buffer
+}
+
+// Read implements io.Reader.
+func (r *reader) Read(p []byte) (n int, err error) {
+	if err = r.ensure(); err != nil {
+		return
+	}
+
+	return r.buf.Read(p)
+}
+
+func (r *reader) ensure() (err error) {
+	if r.buf == nil {
+		r.buf, err = r.buffer()
+	}
+
+	return
+}
+
+func (r *reader) buffer() (*bytes.Buffer, error) {
+	data, err := Marshal(r.message, r.media)
+	if err != nil {
+		return nil, err
+	}
+
+	return bytes.NewBuffer(data), nil
+}
+
+func NewReader(message proto.Message, media tdl.MediaType) io.Reader {
+	return &reader{
+		media:   media,
+		message: message,
+	}
+}
