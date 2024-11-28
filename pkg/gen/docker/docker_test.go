6900b31483d594dee956d319472d0ac6f91bdc72
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-11-28 14:50:20 -0600

    Refactor Docker package and simplify code
    The Docker package has been refactored for better organization. The 'docker.go' file was moved to a new 'docker' subdirectory. The Docker struct was simplified by removing unnecessary fields and methods, and the client is now passed in directly when creating a new instance. This change simplifies the Execute method by removing the need to create a new client or ensure image existence each time it's called. Test files were also updated accordingly to reflect these changes.

--- pkg/gen/docker/docker_test.go
+++ pkg/gen/docker/docker_test.go
@@ -1,4 +1,4 @@
-package gen_test
+package docker_test
 
 import (
 	"context"
@@ -7,21 +7,21 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	"github.com/unstoppablemango/tdl/pkg/gen"
+	"github.com/unstoppablemango/tdl/pkg/gen/docker"
 	. "github.com/unstoppablemango/tdl/pkg/testing/matcher"
 	tdlv1alpha1 "github.com/unstoppablemango/tdl/pkg/unmango/dev/tdl/v1alpha1"
 )
 
 var _ = Describe("Docker", func() {
 	It("should work", func(ctx context.Context) {
-		g := gen.NewDocker("ghcr.io/unstoppablemango/uml2ts:v0.0.30",
-			client.WithAPIVersionNegotiation(),
-		)
+		client, err := client.NewClientWithOpts(client.WithAPIVersionNegotiation())
+		Expect(err).NotTo(HaveOccurred())
+		g := docker.New(client, "ghcr.io/unstoppablemango/uml2ts:v0.0.30")
 		spec := &tdlv1alpha1.Spec{}
 
 		fs, err := g.Execute(ctx, spec)
 
 		Expect(err).NotTo(HaveOccurred())
-		Expect(fs).To(ContainFile("out"))
+		Expect(fs).To(ContainFile("stdout"))
 	})
 })
--- pkg/plugin/docker/plugin.go
+++ pkg/plugin/docker/plugin.go
@@ -2,14 +2,19 @@ package docker
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"slices"
 
+	"github.com/charmbracelet/log"
+	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/client"
 	tdl "github.com/unstoppablemango/tdl/pkg"
-	"github.com/unstoppablemango/tdl/pkg/gen"
+	"github.com/unstoppablemango/tdl/pkg/gen/docker"
 )
 
 type plugin struct {
-	client client.ContainerAPIClient
+	client client.APIClient
 	image  string
 }
 
@@ -19,8 +24,12 @@ func (p *plugin) SinkGenerator(tdl.Target) (tdl.SinkGenerator, error) {
 }
 
 // Generator implements tdl.Plugin.
-func (p *plugin) Generator(context.Context, tdl.Target) (tdl.Generator, error) {
-	return gen.NewDocker(p.image), nil
+func (p *plugin) Generator(ctx context.Context, t tdl.Target) (tdl.Generator, error) {
+	if err := p.ensure(ctx); err != nil {
+		return nil, fmt.Errorf("pulling image: %w", err)
+	}
+
+	return docker.New(p.client, p.image), nil
 }
 
 // String implements tdl.Plugin.
@@ -28,6 +37,43 @@ func (p *plugin) String() string {
 	return p.image
 }
 
-func New(client client.ContainerAPIClient, image string) tdl.Plugin {
+func (p *plugin) ensure(ctx context.Context) error {
+	log.Debug("listing images")
+	images, err := p.client.ImageList(ctx, image.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	log.Debug("searching for existing image",
+		"images", len(images),
+		"image", p.image,
+	)
+	for _, i := range images {
+		if slices.Contains(i.RepoTags, p.image) {
+			log.Debug("image exists")
+			return nil
+		}
+	}
+
+	log.Debug("pulling image")
+	r, err := p.client.ImagePull(ctx, p.image, image.PullOptions{})
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	log.Debug("reading response")
+	data, err := io.ReadAll(r)
+	if err != nil {
+		return err
+	}
+
+	// TODO: This contains a JSON stream
+	// we could use for reporting progress
+	log.Debug(string(data))
+	return nil
+}
+
+func New(client client.APIClient, image string) tdl.Plugin {
 	return &plugin{client, image}
 }
