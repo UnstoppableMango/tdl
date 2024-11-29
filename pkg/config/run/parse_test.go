074650a3438f96b4f615dc6d459af76a4ab65811
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-11-29 15:17:58 -0600

    Enhanced argument parsing in tests
    Added several new test cases to improve the coverage of argument parsing. These include error handling when no arguments are provided, parsing standard input, input files, standard output and output files. This will ensure that our application behaves as expected under different scenarios.

--- pkg/config/run/parse_test.go
+++ pkg/config/run/parse_test.go
@@ -8,6 +8,12 @@ import (
 )
 
 var _ = Describe("Parse", func() {
+	It("should error when no arguments are provided", func() {
+		_, err := run.ParseArgs([]string{})
+
+		Expect(err).To(MatchError("not enough arguments"))
+	})
+
 	It("should parse stdin", func() {
 		config, err := run.ParseArgs([]string{"-"})
 
@@ -15,5 +21,36 @@ var _ = Describe("Parse", func() {
 		Expect(config.Inputs).To(HaveLen(1))
 		input := config.Inputs[0]
 		Expect(input.GetStdin()).To(BeTrue())
+		Expect(input.GetFile()).To(BeNil())
+		Expect(input.GetInline()).To(BeNil())
+	})
+
+	It("should parse input file", func() {
+		config, err := run.ParseArgs([]string{"some-file.txt"})
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(config.Inputs).To(HaveLen(1))
+		input := config.Inputs[0]
+		Expect(input.GetFile()).NotTo(BeNil())
+		Expect(input.GetFile().Path).To(Equal("some-file.txt"))
+		Expect(input.GetStdin()).To(BeFalse())
+		Expect(input.GetInline()).To(BeNil())
+	})
+
+	It("should parse stdout when no output is provided", func() {
+		config, err := run.ParseArgs([]string{"some-file.txt"})
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(config.Output).NotTo(BeNil())
+		Expect(config.GetStdout()).To(BeTrue())
+	})
+
+	It("should parse output file", func() {
+		config, err := run.ParseArgs([]string{"some-file.txt", "output.txt"})
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(config.Output).NotTo(BeNil())
+		Expect(config.GetPath()).To(Equal("output.txt"))
+		Expect(config.GetStdout()).To(BeFalse())
 	})
 })
