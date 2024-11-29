78ef65e82948dd2cbefdc607473c9c376f2ffc12
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-11-29 16:54:44 -0600

    WIP on generator exec

---
+++ pkg/gen/exec.go
@@ -0,0 +1,14 @@
+package gen
+
+import (
+	"context"
+	"errors"
+
+	"github.com/unmango/go/iter"
+	tdl "github.com/unstoppablemango/tdl/pkg"
+	uxv1alpha1 "github.com/unstoppablemango/tdl/pkg/unmango/dev/ux/v1alpha1"
+)
+
+func Execute(ctx context.Context, config *uxv1alpha1.RunConfig, plugins iter.Seq[tdl.Plugin]) error {
+	return errors.New("unimplemented")
+}
