28b5625a2df6a1dc663b3c487815f5305903838e
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-11-29 23:14:15 -0600

    Refactor plugin stuff

--- pkg/gen/exec.go
+++ pkg/gen/exec.go
@@ -9,6 +9,6 @@ import (
 	uxv1alpha1 "github.com/unstoppablemango/tdl/pkg/unmango/dev/ux/v1alpha1"
 )
 
-func Execute(ctx context.Context, config *uxv1alpha1.RunConfig, plugins iter.Seq[tdl.Plugin]) error {
+func Execute(ctx context.Context, config *uxv1alpha1.RunConfig, plugins iter.Seq[tdl.GeneratorPlugin]) error {
 	return errors.New("unimplemented")
 }
