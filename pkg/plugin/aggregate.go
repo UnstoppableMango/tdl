bbfb3f7ebc1b6af2b5921afd1043170389414b57
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-12-04 13:03:56 -0600

    Fix linter errors

--- pkg/plugin/aggregate.go
+++ pkg/plugin/aggregate.go
@@ -2,7 +2,6 @@ package plugin
 
 import (
 	"fmt"
-	"iter"
 
 	tdl "github.com/unstoppablemango/tdl/pkg"
 )
@@ -30,20 +29,6 @@ func (a Aggregate) Supports(target tdl.Target) bool {
 // String implements tdl.Plugin.
 func (a Aggregate) String() string {
 	return fmt.Sprintf("%v", []tdl.Plugin(a))
-}
-
-func (a Aggregate) sorted() []Ordered {
-	return Sorted(a.ordered())
-}
-
-func (a Aggregate) ordered() iter.Seq[Ordered] {
-	return func(yield func(Ordered) bool) {
-		for _, p := range a {
-			if !yield(AsOrdered(p)) {
-				return
-			}
-		}
-	}
 }
 
 func NewAggregate(plugins ...tdl.Plugin) Aggregate {
