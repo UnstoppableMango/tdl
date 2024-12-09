9e5f323802fc9d67de2333d8cdd2ddecce6a2085
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-12-09 14:28:52 -0600

    Test cache

--- pkg/cache/fs.go
+++ pkg/cache/fs.go
@@ -8,9 +8,7 @@ import (
 	tdl "github.com/unstoppablemango/tdl/pkg"
 )
 
-type Fs struct {
-	afero.Fs
-}
+type Fs struct{ afero.Fs }
 
 // Get implements tdl.Cache.
 func (f *Fs) Get(key string) (*tdl.CacheItem, error) {
@@ -34,4 +32,23 @@ var _ tdl.Cache = &Fs{}
 
 func NewFs(fs afero.Fs) *Fs {
 	return &Fs{fs}
+}
+
+func NewFsAt(fs afero.Fs, path string) *Fs {
+	return NewFs(afero.NewBasePathFs(fs, path))
+}
+
+func NewMemFs() *Fs {
+	return NewFs(afero.NewMemMapFs())
+}
+
+func NewTmpFs() (*Fs, error) {
+	fs := afero.NewOsFs()
+	tmp, err := afero.TempDir(fs, "", "")
+	if err != nil {
+		return nil, fmt.Errorf("creating tmp dir: %w", err)
+	}
+
+	// TODO: Maybe implement io.Closer to delete the tmp dir
+	return NewFsAt(fs, tmp), nil
 }
--- pkg/testing/cache.go
+++ pkg/testing/cache.go
@@ -1,24 +1,28 @@
 package testing
 
 import (
+	"io"
 	"testing"
 
+	"github.com/onsi/ginkgo/v2"
 	g "github.com/onsi/gomega"
 	"github.com/spf13/afero"
-	"github.com/unstoppablemango/tdl/pkg/plugin/cache"
+	tdl "github.com/unstoppablemango/tdl/pkg"
+	"github.com/unstoppablemango/tdl/pkg/cache"
+	pcache "github.com/unstoppablemango/tdl/pkg/plugin/cache"
 )
 
-type DirCache struct{ cache.Directory }
+type DirCache struct{ pcache.Directory }
 
 func (c *DirCache) Dir() string {
 	return c.JoinPath()
 }
 
-var _ cache.Directory = &DirCache{}
+var _ pcache.Directory = &DirCache{}
 
 func NewCacheForT(t *testing.T) *DirCache {
 	return &DirCache{
-		cache.AtDirectory(t.TempDir()),
+		pcache.AtDirectory(t.TempDir()),
 	}
 }
 
@@ -38,5 +42,40 @@ func NewCache(fsys afero.Fs) *DirCache {
 	path, err := afero.TempDir(fsys, "", "")
 	g.Expect(err).NotTo(g.HaveOccurred())
 
-	return &DirCache{cache.NewDirectory(fsys, path)}
+	return &DirCache{pcache.NewDirectory(fsys, path)}
+}
+
+type Cache struct {
+	GetFunc    func(string) (*tdl.CacheItem, error)
+	WriterFunc func(string) (io.WriteCloser, error)
+}
+
+func (c *Cache) Get(key string) (*tdl.CacheItem, error) {
+	if c.GetFunc == nil {
+		panic("unimplemented")
+	}
+
+	return c.GetFunc(key)
+}
+
+func (c *Cache) Writer(key string) (io.WriteCloser, error) {
+	if c.WriterFunc == nil {
+		panic("unimplemented")
+	}
+
+	return c.WriterFunc(key)
+}
+
+func NewCacheFrom(cache tdl.Cache) *Cache {
+	return &Cache{
+		GetFunc:    cache.Get,
+		WriterFunc: cache.Writer,
+	}
+}
+
+func NewTmpCache(t ginkgo.GinkgoTInterface) (*Cache, string) {
+	tmp := t.TempDir()
+	fs := afero.NewBasePathFs(afero.NewOsFs(), tmp)
+
+	return NewCacheFrom(cache.NewFs(fs)), tmp
 }
