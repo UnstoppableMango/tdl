3d5cf6a5693920923e90980e94e562371e8eecd7
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-12-10 18:36:05 -0600

    Added tar extraction functionality and tests
    Introduced a new function to extract tar files. This function reads the tar file, checks if the bin exists, and writes it if not. Accompanying unit tests have been added to ensure correct functionality. Additionally, embedded test data has been included in the cache test suite for more comprehensive testing.

---
+++ pkg/cache/archive_test.go
@@ -0,0 +1,33 @@
+package cache_test
+
+import (
+	"io"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	. "github.com/unmango/go/testing/matcher"
+
+	"github.com/unstoppablemango/tdl/pkg/cache"
+)
+
+var _ = Describe("Archive", func() {
+	var archive io.Reader
+
+	BeforeEach(func() {
+		var err error
+		archive, err = testdata.Open("testdata/tdl-linux-amd64.tar.gz")
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("should extract tar", func() {
+		c := cache.NewMemFs()
+		b := cache.NewMemFs()
+
+		err := cache.ExtractTar(c, b, "tdl-linux-amd64.tar.gz", archive)
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(c).To(ContainFile("tdl-linux-amd64.tar.gz"))
+		Expect(b).To(ContainFile("uml2ts"))
+		Expect(b).To(ContainFile("ux"))
+	})
+})
--- pkg/cache/cache_suite_test.go
+++ pkg/cache/cache_suite_test.go
@@ -1,11 +1,15 @@
 package cache_test
 
 import (
+	"embed"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
+
+//go:embed testdata
+var testdata embed.FS
 
 func TestCache(t *testing.T) {
 	RegisterFailHandler(Fail)
