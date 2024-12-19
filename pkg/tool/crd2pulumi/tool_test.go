1acd34ba9739610e21e0f92d2999e4fa327edcaa
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-12-18 19:31:10 -0600

    Refactor test setup in tool_test.go
    The test setup for the "Tool" tests has been refactored. The variable declaration and initialization previously done at the top level of the "Tool" describe block have been moved into a new nested "Execute" describe block. This change ensures that these setup steps are only run for the relevant tests, improving efficiency and readability.

--- pkg/tool/crd2pulumi/tool_test.go
+++ pkg/tool/crd2pulumi/tool_test.go
@@ -14,15 +14,6 @@ import (
 )
 
 var _ = Describe("Tool", func() {
-	var crdyaml []byte
-
-	BeforeEach(func() {
-		log.SetLevel(log.ErrorLevel)
-		var err error
-		crdyaml, err = testdata.ReadFile("testdata/objectbucket.io_objectbucketclaims.yaml")
-		Expect(err).NotTo(HaveOccurred())
-	})
-
 	AfterEach(func() {
 		log.SetLevel(log.InfoLevel)
 	})
@@ -39,23 +30,35 @@ var _ = Describe("Tool", func() {
 		},
 	)
 
-	FIt("should execute", Label("E2E"), func(ctx context.Context) {
-		t := crd2pulumi.Tool{
-			Path: toolPath,
-			Options: crd2pulumi.Options{
-				NodeJS: &crd2pulumi.LangOptions{
-					Enabled: true,
+	Describe("Execute", func() {
+		var crdyaml []byte
+
+		BeforeEach(func() {
+			log.SetLevel(log.ErrorLevel)
+			var err error
+			crdyaml, err = testdata.ReadFile("testdata/objectbucket.io_objectbucketclaims.yaml")
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		FIt("should execute", Label("E2E"), func(ctx context.Context) {
+			t := crd2pulumi.Tool{
+				Path: toolPath,
+				Options: crd2pulumi.Options{
+					NodeJS: &crd2pulumi.LangOptions{
+						Enabled: true,
+					},
 				},
-			},
-		}
-		fs := afero.NewMemMapFs()
-		err := afero.WriteFile(fs, "blah.yaml", crdyaml, os.ModePerm)
-		Expect(err).NotTo(HaveOccurred())
+			}
+			fs := afero.NewMemMapFs()
+			err := afero.WriteFile(fs, "blah.yaml", crdyaml, os.ModePerm)
+			Expect(err).NotTo(HaveOccurred())
 
-		out, err := t.Execute(ctx, fs)
+			out, err := t.Execute(ctx, fs)
 
-		Expect(err).NotTo(HaveOccurred())
-		Expect(afero.IsEmpty(out, "")).To(BeFalseBecause("the tool generated files"))
-		Expect(out).To(ContainFile("nodejs/package.json"))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(afero.IsEmpty(out, "")).To(BeFalseBecause("the tool generated files"))
+			Expect(out).To(ContainFile("nodejs/package.json"))
+			Expect(out).To(ContainFile("nodejs/index.ts"))
+		})
 	})
 })
