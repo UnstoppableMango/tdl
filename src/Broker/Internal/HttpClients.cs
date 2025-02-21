74997fa9ef2ff6202f17ef374bdec0edf4432fb0
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-02-21 16:08:48 -0600

    Cleaning the old stuff

--- src/Broker/Internal/HttpClients.cs
+++
@@ -1,6 +0,0 @@
-namespace UnMango.Tdl.Broker.Internal;
-
-internal static class HttpClients
-{
-	public const string GitHub = "github";
-}
--- src/Cli/Docker/IDockerProgress.cs
+++
@@ -1,6 +0,0 @@
-using Docker.DotNet.Models;
-
-namespace UnMango.Tdl.Cli.Docker;
-
-internal interface IDockerProgress : IProgress<string>, IProgress<JSONMessage>;
-
