a282c4db7f3511820b8be75644b94a8c486fbff0
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-02-21 23:24:21 -0600

    Whole buncha type things

---
+++ src/Lang.Tests/Ast.fs
@@ -0,0 +1,24 @@
+﻿module Ast
+
+open Swensen.Unquote
+open UnMango.Tdl
+open UnMango.Tdl.Ast
+open Xunit
+
+[<Fact>]
+let ``Should convert a type`` () =
+  let expected = Type()
+  expected.Type_ <- "Test"
+
+  test <@ Type.proto { Name = "Test" } = Type() @>
+
+[<Fact>]
+let ``Should convert an empty spec`` () =
+  test <@ Spec.proto { Types = [] } = Spec() @>
+
+[<Fact>]
+let ``Should convert a spec`` () =
+  let expected = Spec()
+  expected.Types_.Add([ "Test", Type() ] |> dict)
+
+  test <@ Spec.proto { Types = [ { Name = "Test" } ] } = expected @>
