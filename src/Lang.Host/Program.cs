547dc9ab86ac60617ad3752a010933c776ca08f1
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-02-22 12:29:23 -0600

    Reflection and sockets

--- src/Lang.Host/Program.cs
+++ src/Lang.Host/Program.cs
@@ -1,11 +1,19 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using UnMango.Tdl.Lang.Host.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel(options => {
+	if (args.Length > 0) {
+		options.ListenUnixSocket(args[0], listen => { listen.Protocols = HttpProtocols.Http2; });
+	}
+});
 
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
 
+app.MapGrpcReflectionService();
 app.MapGrpcService<ParserService>();
 
 app.Run();
