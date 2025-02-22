942cf89d27f1d1b7f47d5964e3bf04c3eddcee99
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-02-21 18:33:32 -0600

    A small type parser

---
+++ src/Lang.Tests/Parser.fs
@@ -0,0 +1,19 @@
+﻿module Parser
+
+open FParsec
+open Swensen.Unquote
+open UnMango.Tdl.Parser
+open Xunit
+
+let success =
+  function
+  | Success(res, _, _) -> res
+  | Failure(msg, _, _) -> failwith msg
+
+[<Fact>]
+let ``Should parse empty spec`` () =
+  test <@ parse "" |> success = { Types = [] } @>
+
+[<Fact>]
+let ``Should parse an empty type`` () =
+  test <@ parse "type Test" |> success = { Types = [{ Name = "Test" }] } @>
