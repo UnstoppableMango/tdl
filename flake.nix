6f5e3e53bf83f0b9fd1f5b71c87d014318bb4a1e
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-09-03 23:40:09 -0500

    chore(main): release 0.1.4 (#721)

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
       perSystem =
         { pkgs, system, ... }:
         let
-          version = "0.1.3"; # x-release-please-version
+          version = "0.1.4"; # x-release-please-version
           go = pkgs.go_1_27;
         in
         {
